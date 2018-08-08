625c20b63cd5efb04ac951bc571fc22ac7f39235
Andrey Parfenov <a1994ndrey@gmail.com>
2018-08-09 00:09:07 +0300

    move emulator mode build to the same bash file
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- cmake_build.sh
+++ cmake_build.sh
@@ -4,8 +4,18 @@ if [ -d build ]; then
   	rm -rf build
 fi
 mkdir build
-
 cd build
-cmake ..
+
+if [[ "$@" == "EMULATOR" ]]
+then
+	echo "Build emulator mode"
+    cmake -DEMULATOR=1 ..
+else
+	echo "Build plain"
+    cmake ..
+fi
+
 cmake --build .
 cd ..
+
+cp ./compiled/libBoardController.so ./python/brainflow/lib/libBoardController.so
