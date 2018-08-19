ebc76249cb48124eca58755994c0128737a784b1
Andrey Parfenov <a1994ndrey@gmail.com>
2018-08-19 16:45:53 +0300

    add data preprocessing to python example
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- python/examples/brainflow_get_data.py
+++ python/examples/brainflow_get_data.py
@@ -1,64 +1,92 @@
 import time
 import numpy as np
 import pandas as pd
-import matplotlib
-matplotlib.use ('Agg')
-import matplotlib.pyplot as plt
 import argparse
+import scipy
+from scipy import signal
+from scipy.fftpack import fft
 
 from brainflow import *
 
 float_formatter = lambda x: "%.5f" % x
 numpy.set_printoptions (formatter={'float_kind': float_formatter}, threshold=numpy.inf)
 
-class CythonDataHandler (object):
+class DataHandler (object):
 
-    def __init__ (self, numpy_data):
+    def __init__ (self, board_type, numpy_data):
+        if board_type == Boards.Cython:
+            self.fs_hz = 250
+            self.num_eeg_channels = 8
+        else:
+            raise Exception ('Unsupported Board Type')
         self.numpy_data = numpy_data
 
         columns = ['package_num']
-        for i in range (8):
+        for i in range (self.num_eeg_channels):
             columns.append ('eeg%d' % (i + 1))
         for i in range (3):
             columns.append ('accel%d' % (i + 1))
         columns.append ('timestamp')
+        self.eeg_df = pd.DataFrame (self.numpy_data, columns = columns, dtype = np.float64)
 
-        self.pandas_df = pd.DataFrame (self.numpy_data, columns = columns, dtype = np.float64)
+    def remove_dc_offset (self):
+        hp_cutoff_hz = 1.0
+        b, a = signal.butter (2, hp_cutoff_hz / (self.fs_hz / 2.0), 'highpass')
 
-    def draw_data (self, filename):
-        eeg_df = self.pandas_df[['eeg1', 'eeg2', 'eeg3', 'eeg4', 'eeg5', 'eeg6', 'eeg7', 'eeg8', 'timestamp']]
-        eeg_df.plot (x = 'timestamp')
-        fig = plt.gcf ()
-        fig.savefig (filename)
+        for i in range (self.num_eeg_channels):
+            column_name = 'eeg%d' % (i + 1)
+            self.eeg_df[column_name] = signal.lfilter (b, a, self.eeg_df[column_name], 0)
+
+    def notch_interference (self):
+        notch_freq_hz = np.array ([50.0, 60.0])
+        for freq_hz in np.nditer (notch_freq_hz):
+            bp_stop_hz = freq_hz + 3.0 * np.array ([-1, 1])
+            b, a = signal.butter (3, bp_stop_hz / (self.fs_hz / 2.0), 'bandstop')
+
+            for i in range (self.num_eeg_channels):
+                column_name = 'eeg%d' % (i + 1)
+                self.eeg_df[column_name] = signal.lfilter (b, a, self.eeg_df[column_name], 0)
+
+    def bandpass (self, start, stop):
+        bp_hz = np.array ([start,stop])
+        b, a = signal.butter (3, bp_hz / (self.fs_hz / 2.0), 'bandpass')
+
+        for i in range (self.num_eeg_channels):
+            column_name = 'eeg%d' % (i + 1)
+            self.eeg_df[column_name] = signal.lfilter (b, a, self.eeg_df[column_name], 0)
+
+    def calculate_fft (self):
+        for i in range (self.num_eeg_channels):
+            column_name = 'eeg%d' % (i + 1)
+            self.eeg_df['fft_' + column_name] = fft (self.eeg_df[column_name])
+
+    def get_data (self):
+        return self.eeg_df
+
+    def preprocess_data (self, start, stop):
+        self.remove_dc_offset ()
+        self.notch_interference ()
+        self.bandpass (start, stop)
+        self.calculate_fft ()
+        return self.get_data ()
 
 
 def main ():
     parser = argparse.ArgumentParser ()
-    parser.add_argument ('--port', type = str, help  = "port name", required = True)
+    parser.add_argument ('--port', type = str, help  = 'port name', required = True)
     args = parser.parse_args ()
 
     board = CythonBoard (args.port.encode ())
     board.prepare_session ()
     board.start_stream ()
-    time.sleep (15)
-    # get last package
-    print (board.get_immediate_board_data ())
-    # get last 250 datapoints
-    current_data = board.get_current_board_data (250)
-    # get all data
+    time.sleep (5)
     data = board.get_board_data ()
     board.stop_stream ()
     board.release_session ()
 
-    data_handler = CythonDataHandler (data)
-    print (data_handler.pandas_df)
-    data_handler.draw_data ('data.png')
-
-    with open ('current_data.raw', 'wt') as f:
-        f.write (str (current_data))
-
-    with open ('all_data.raw', 'wt') as f:
-        f.write (str (data))
+    data_handler = DataHandler (Boards.Cython, data)
+    filtered_data = data_handler.preprocess_data (1, 50)
+    filtered_data.to_csv ('results.csv')
 
 
 if __name__ == "__main__":
