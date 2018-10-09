2c353fa326a07e33e9b3c18a9edb2787680f01f2
Andrey1994 <a1994ndrey@gmail.com>
2018-10-10 01:29:24 +0300

    add windows support for java packages
    Signed-off-by: Andrey1994 <a1994ndrey@gmail.com>

--- cmake_build.cmd
+++ cmake_build.cmd
@@ -16,3 +16,6 @@ echo F | xcopy /Y .\src\board_controller\inc\board_controller.h .\cpp-package\in
 echo F | xcopy /Y .\compiled\Debug\BoardController.dll .\matlab-package\brainflow\lib\BoardController.dll
 echo F | xcopy /Y .\compiled\Debug\BoardController.lib .\matlab-package\brainflow\lib\BoardController.lib
 echo F | xcopy /Y .\src\board_controller\inc\board_controller.h .\matlab-package\brainflow\inc\board_controller.h
+
+echo F | xcopy /Y .\compiled\Debug\BoardController.dll .\java-package\brainflow\src\main\resources\BoardController.dll
+echo F | xcopy /Y .\compiled\Debug\BoardController.lib .\java-package\brainflow\src\main\resources\BoardController.lib
