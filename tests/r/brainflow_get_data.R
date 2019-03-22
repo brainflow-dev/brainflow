15ea93f10f114da00395988dc2a240971d23c358
aparfenov <aparfenov@nvidia.com>
2019-03-22 05:10:12 -0400

    add R test
    Signed-off-by: aparfenov <aparfenov@nvidia.com>

---
+++ tests/r/brainflow_get_data.R
@@ -0,0 +1,17 @@
+library(brainflow)
+
+args = commandArgs (trailingOnly = TRUE)
+board_shim <- get_board_shim(Boards()$Cython["Type"], args[1])
+prepare_session(board_shim)
+start_stream(board_shim, 3600)
+Sys.sleep(time = 5)
+stop_stream(board_shim)
+data <- get_current_board_data(board_shim, 250)
+release_session(board_shim)
+
+data_handler <- get_data_handler(Boards()$Cython["Type"], numpy_data = data)
+preprocess_data(data_handler, 3, 1, 50, TRUE)
+preprocessed_data <- get_data(data_handler)
+head(preprocessed_data)
+save_csv(data_handler, "results.csv")
+head(get_data(get_data_handler(Boards()$Cython["Type"], csv_file = "results.csv")))
