0d426aff58d7ee8353c6f6811765d8def221333a
Andrey Parfenov <a1994ndrey@gmail.com>
2019-05-17 21:44:22 +0300

    clang-format (#7)
    * add clang-format file, style fix
    
    Signed-off-by: Andrey1994 <a1994ndrey@gmail.com>

--- src/board_controller/inc/board.h
+++ src/board_controller/inc/board.h
@@ -3,20 +3,20 @@
 
 #include "spdlog/spdlog.h"
 
-
 class Board
 {
-    public:
-        static std::shared_ptr<spdlog::logger> board_logger;
-        static int set_log_level (int level);
+public:
+    static std::shared_ptr<spdlog::logger> board_logger;
+    static int set_log_level (int level);
 
-        virtual int prepare_session () = 0;
-        virtual int start_stream (int buffer_size) = 0;
-        virtual int stop_stream () = 0;
-        virtual int release_session () = 0;
-        virtual int get_current_board_data (int num_samples, float *data_buf, double *ts_buf, int *returned_samples) = 0;
-        virtual int get_board_data_count (int *result) = 0;
-        virtual int get_board_data (int data_count, float *data_buf, double *ts_buf) = 0;
+    virtual int prepare_session () = 0;
+    virtual int start_stream (int buffer_size) = 0;
+    virtual int stop_stream () = 0;
+    virtual int release_session () = 0;
+    virtual int get_current_board_data (
+        int num_samples, float *data_buf, double *ts_buf, int *returned_samples) = 0;
+    virtual int get_board_data_count (int *result) = 0;
+    virtual int get_board_data (int data_count, float *data_buf, double *ts_buf) = 0;
 };
 
 #endif
