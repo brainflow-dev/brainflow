346abe1d0223115a010694f8606de74eceb8d4d1
Andrey Parfenov <a1994ndrey@gmail.com>
2019-07-12 01:26:51 +0200

    add 32bit for python and c#
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- csharp-package/brainflow/brainflow/library.cs
+++ csharp-package/brainflow/brainflow/library.cs
@@ -31,24 +31,115 @@ namespace brainflow
         GANGLION_BOARD = 1
     };
 
-    public static class Library
+
+    // lib name is const in DllImport directive, so I can not use only one class
+    // alternative solutios: call loadlibrary from kernel32.dll
+    // or rename libs to have the same name and load them from different folders but I dont want to fix it in all bindings
+    // so there is no really good solution
+    public static class Library64
     {
-        [DllImport("BoardController.dll", SetLastError = true)]
-        public static extern int prepare_session(int board_id, string port_name);
-        [DllImport("BoardController.dll", SetLastError = true)]
-        public static extern int start_stream(int buffer_size);
-        [DllImport("BoardController.dll", SetLastError = true)]
-        public static extern int stop_stream();
-        [DllImport("BoardController.dll", SetLastError = true)]
-        public static extern int release_session();
-        [DllImport("BoardController.dll", SetLastError = true)]
-        public static extern int get_current_board_data(int num_samples, float[] data_buf, double[] ts_buf, int[] returned_samples);
-        [DllImport("BoardController.dll", SetLastError = true)]
+        [DllImport ("BoardController.dll", SetLastError = true)]
+        public static extern int prepare_session (int board_id, string port_name);
+        [DllImport ("BoardController.dll", SetLastError = true)]
+        public static extern int start_stream (int buffer_size);
+        [DllImport ("BoardController.dll", SetLastError = true)]
+        public static extern int stop_stream ();
+        [DllImport ("BoardController.dll", SetLastError = true)]
+        public static extern int release_session ();
+        [DllImport ("BoardController.dll", SetLastError = true)]
+        public static extern int get_current_board_data (int num_samples, float[] data_buf, double[] ts_buf, int[] returned_samples);
+        [DllImport ("BoardController.dll", SetLastError = true)]
         public static extern int get_board_data_count(int[] result);
-        [DllImport("BoardController.dll", SetLastError = true)]
-        public static extern int get_board_data(int data_count, float[] data_buf, double[] ts_buf);
+        [DllImport ("BoardController.dll", SetLastError = true)]
+        public static extern int get_board_data (int data_count, float[] data_buf, double[] ts_buf);
         [DllImport ("BoardController.dll", SetLastError = true)]
         public static extern int set_log_level (int log_level);
     }
 
+    public static class Library32
+    {
+        [DllImport ("BoardController32.dll", SetLastError = true)]
+        public static extern int prepare_session (int board_id, string port_name);
+        [DllImport ("BoardController32.dll", SetLastError = true)]
+        public static extern int start_stream (int buffer_size);
+        [DllImport ("BoardController32.dll", SetLastError = true)]
+        public static extern int stop_stream ();
+        [DllImport ("BoardController32.dll", SetLastError = true)]
+        public static extern int release_session ();
+        [DllImport ("BoardController32.dll", SetLastError = true)]
+        public static extern int get_current_board_data (int num_samples, float[] data_buf, double[] ts_buf, int[] returned_samples);
+        [DllImport ("BoardController32.dll", SetLastError = true)]
+        public static extern int get_board_data_count (int[] result);
+        [DllImport ("BoardController32.dll", SetLastError = true)]
+        public static extern int get_board_data (int data_count, float[] data_buf, double[] ts_buf);
+        [DllImport ("BoardController32.dll", SetLastError = true)]
+        public static extern int set_log_level (int log_level);
+    }
+
+    public static class Library
+    {
+        public static int prepare_session (int board_id, string port_name)
+        {
+            if (System.Environment.Is64BitProcess)
+                return Library64.prepare_session (board_id, port_name);
+            else
+                return Library32.prepare_session (board_id, port_name);
+        }
+
+        public static int start_stream (int buffer_size)
+        {
+            if (System.Environment.Is64BitProcess)
+                return Library64.start_stream (buffer_size);
+            else
+                return Library32.start_stream (buffer_size);
+        }
+
+        public static int stop_stream ()
+        {
+            if (System.Environment.Is64BitProcess)
+                return Library64.stop_stream ();
+            else
+                return Library32.stop_stream ();
+        }
+
+        public static int release_session ()
+        {
+            if (System.Environment.Is64BitProcess)
+                return Library64.release_session ();
+            else
+                return Library32.release_session ();
+        }
+
+        public static int get_current_board_data (int num_samples, float[] data_buf, double[] ts_buf, int[] returned_samples)
+        {
+            if (System.Environment.Is64BitProcess)
+                return Library64.get_current_board_data (num_samples, data_buf, ts_buf, returned_samples);
+            else
+                return Library32.get_current_board_data (num_samples, data_buf, ts_buf, returned_samples);
+        }
+
+        public static int get_board_data_count (int[] result)
+        {
+            if (System.Environment.Is64BitProcess)
+                return Library64.get_board_data_count (result);
+            else
+                return Library32.get_board_data_count (result);
+        }
+
+        public static int get_board_data (int data_count, float[] data_buf, double[] ts_buf)
+        {
+            if (System.Environment.Is64BitProcess)
+                return Library64.get_board_data (data_count, data_buf, ts_buf);
+            else
+                return Library32.get_board_data (data_count, data_buf, ts_buf);
+        }
+
+        public static int set_log_level (int log_level)
+        {
+            if (System.Environment.Is64BitProcess)
+                return Library64.set_log_level (log_level);
+            else
+                return Library32.set_log_level (log_level);
+        }
+    }
 }
