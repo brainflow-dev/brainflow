c56cd89d038a5020cfad7c94f958becbf9d0bcff
Andrey Parfenov <a1994ndrey@gmail.com>
2019-09-26 10:43:50 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,8 @@ extensions = [
     'sphinx.ext.autodoc'
 ]
 
+autodoc_member_order = 'bysource'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
