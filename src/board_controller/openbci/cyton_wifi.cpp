aa5b0de41746ebf8ebd13d9d486aad7e99d67eda
Andrey Parfenov <a1994ndrey@gmail.com>
2020-05-12 22:35:00 +0300

    improve timestamps for wifi shield (#42)
    * improve timestamps for wifi shield
    
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- src/board_controller/openbci/cyton_wifi.cpp
+++ src/board_controller/openbci/cyton_wifi.cpp
@@ -2,6 +2,10 @@
 #include "custom_cast.h"
 #include "timestamp.h"
 
+#ifndef _WIN32
+#include <errno.h>
+#endif
+
 
 #define START_BYTE 0xA0
 #define END_BYTE_STANDARD 0xC0
@@ -37,79 +41,85 @@ void CytonWifi::read_thread ()
         Byte 33: 0xCX where X is 0-F in hex
     */
     int res;
-    unsigned char b[OpenBCIWifiShieldBoard::transaction_size];
+    unsigned char b[OpenBCIWifiShieldBoard::package_size];
     double accel[3] = {0.};
     while (keep_alive)
     {
         // check start byte
-        res = server_socket->recv (b, OpenBCIWifiShieldBoard::transaction_size);
-        if (res != OpenBCIWifiShieldBoard::transaction_size)
+        res = server_socket->recv (b, OpenBCIWifiShieldBoard::package_size);
+        if (res != OpenBCIWifiShieldBoard::package_size)
+        {
+            safe_logger (spdlog::level::warn, "recv result: {}", res);
+            if (res == -1)
+            {
+#ifdef _WIN32
+                safe_logger (spdlog::level::warn, "WSAGetLastError is {}", WSAGetLastError ());
+#else
+                safe_logger (spdlog::level::warn, "errno {} message {}", errno, strerror (errno));
+#endif
+            }
+
+            continue;
+        }
+
+        if (b[0] != START_BYTE)
         {
             continue;
         }
-        for (int cur_package = 0;
-             cur_package < OpenBCIWifiShieldBoard::num_packages_per_transaction; cur_package++)
+        unsigned char *bytes = b + 1; // for better consistency between plain cyton and wifi, in
+                                      // plain cyton index is shifted by 1
+
+        if ((bytes[31] < END_BYTE_STANDARD) || (bytes[31] > END_BYTE_MAX))
         {
-            int offset = cur_package * OpenBCIWifiShieldBoard::package_size;
-            if (b[0 + offset] != START_BYTE)
-            {
-                continue;
-            }
-            unsigned char *bytes = b + 1; // for better consistency between plain cyton and wifi, in
-                                          // plain cyton index is shifted by 1
-
-            if ((bytes[31 + offset] < END_BYTE_STANDARD) || (bytes[31 + offset] > END_BYTE_MAX))
-            {
-                safe_logger (spdlog::level::warn, "Wrong end byte {}", bytes[31 + offset]);
-                continue;
-            }
-
-            double package[22] = {0.};
-            // package num
-            package[0] = (double)bytes[0 + offset];
-            // eeg
-            for (int i = 0; i < 8; i++)
-            {
-                package[i + 1] = eeg_scale * cast_24bit_to_int32 (bytes + 1 + 3 * i + offset);
-            }
-            package[12] = (double)bytes[31 + offset]; // end byte
-            // place unprocessed bytes for all modes to other_channels
-            package[13] = (double)bytes[25 + offset];
-            package[14] = (double)bytes[26 + offset];
-            package[15] = (double)bytes[27 + offset];
-            package[16] = (double)bytes[28 + offset];
-            package[17] = (double)bytes[29 + offset];
-            package[18] = (double)bytes[30 + offset];
-            // place processed bytes for accel
-            if (bytes[31 + offset] == END_BYTE_STANDARD)
-            {
-                int32_t accel_temp[3] = {0};
-                accel_temp[0] = cast_16bit_to_int32 (bytes + 25 + offset);
-                accel_temp[1] = cast_16bit_to_int32 (bytes + 27 + offset);
-                accel_temp[2] = cast_16bit_to_int32 (bytes + 29 + offset);
-
-                if (accel_temp[0] != 0)
-                {
-                    accel[0] = accel_scale * accel_temp[0];
-                    accel[1] = accel_scale * accel_temp[1];
-                    accel[2] = accel_scale * accel_temp[2];
-                }
-
-                package[9] = accel[0];
-                package[10] = accel[1];
-                package[11] = accel[2];
-            }
-            // place processed bytes for analog
-            if (bytes[31 + offset] == END_BYTE_ANALOG)
-            {
-                package[19] = cast_16bit_to_int32 (bytes + 25 + offset);
-                package[20] = cast_16bit_to_int32 (bytes + 27 + offset);
-                package[21] = cast_16bit_to_int32 (bytes + 29 + offset);
-            }
-
-            double timestamp = get_timestamp ();
-            db->add_data (timestamp, package);
-            streamer->stream_data (package, 22, timestamp);
+            safe_logger (spdlog::level::warn, "Wrong end byte {}", bytes[31]);
+            continue;
         }
+
+        double package[22] = {0.};
+        // package num
+        package[0] = (double)bytes[0];
+        // eeg
+        for (int i = 0; i < 8; i++)
+        {
+            package[i + 1] = eeg_scale * cast_24bit_to_int32 (bytes + 1 + 3 * i);
+        }
+        package[12] = (double)bytes[31]; // end byte
+        // place unprocessed bytes for all modes to other_channels
+        package[13] = (double)bytes[25];
+        package[14] = (double)bytes[26];
+        package[15] = (double)bytes[27];
+        package[16] = (double)bytes[28];
+        package[17] = (double)bytes[29];
+        package[18] = (double)bytes[30];
+        // place processed bytes for accel
+        if (bytes[31] == END_BYTE_STANDARD)
+        {
+            int32_t accel_temp[3] = {0};
+            accel_temp[0] = cast_16bit_to_int32 (bytes + 25);
+            accel_temp[1] = cast_16bit_to_int32 (bytes + 27);
+            accel_temp[2] = cast_16bit_to_int32 (bytes + 29);
+
+            if (accel_temp[0] != 0)
+            {
+                accel[0] = accel_scale * accel_temp[0];
+                accel[1] = accel_scale * accel_temp[1];
+                accel[2] = accel_scale * accel_temp[2];
+            }
+
+            package[9] = accel[0];
+            package[10] = accel[1];
+            package[11] = accel[2];
+        }
+        // place processed bytes for analog
+        if (bytes[31] == END_BYTE_ANALOG)
+        {
+            package[19] = cast_16bit_to_int32 (bytes + 25);
+            package[20] = cast_16bit_to_int32 (bytes + 27);
+            package[21] = cast_16bit_to_int32 (bytes + 29);
+        }
+
+        double timestamp = get_timestamp ();
+        db->add_data (timestamp, package);
+        streamer->stream_data (package, 22, timestamp);
     }
 }
--- src/board_controller/openbci/ganglion_wifi.cpp
+++ src/board_controller/openbci/ganglion_wifi.cpp
@@ -2,12 +2,160 @@
 #include "custom_cast.h"
 #include "timestamp.h"
 
+#include "http.h"
+
+#ifndef _WIN32
+#include <errno.h>
+#endif
 
 #define START_BYTE 0xA0
 #define END_BYTE_STANDARD 0xC0
 #define END_BYTE_ANALOG 0xC1
 #define END_BYTE_MAX 0xC6
 
+
+int GanglionWifi::config_board (char *config)
+{
+    if ((config == NULL) || (strlen (config) == 0))
+    {
+        return INVALID_ARGUMENTS_ERROR;
+    }
+    if ((keep_alive) && ((config[0] == 'z') || (config[0] == 'Z')))
+    {
+        safe_logger (
+            spdlog::level::err, "For Ganglion WIFI impedance works only if streaming is stopped");
+        return INVALID_ARGUMENTS_ERROR;
+    }
+    if (config[0] == 'z')
+    {
+        is_cheking_impedance = true;
+        return STATUS_OK;
+    }
+    if (config[0] == 'Z')
+    {
+        is_cheking_impedance = false;
+        return STATUS_OK;
+    }
+    return send_config (config);
+}
+
+int GanglionWifi::start_stream (int buffer_size, char *streamer_params)
+{
+    if (keep_alive)
+    {
+        safe_logger (spdlog::level::err, "Streaming thread already running");
+        return STREAM_ALREADY_RUN_ERROR;
+    }
+    if (buffer_size <= 0 || buffer_size > MAX_CAPTURE_SAMPLES)
+    {
+        safe_logger (spdlog::level::err, "invalid array size");
+        return INVALID_BUFFER_SIZE_ERROR;
+    }
+
+    if (db)
+    {
+        delete db;
+        db = NULL;
+    }
+    if (streamer)
+    {
+        delete streamer;
+        streamer = NULL;
+    }
+
+    int res = prepare_streamer (streamer_params);
+    if (res != STATUS_OK)
+    {
+        return res;
+    }
+    db = new DataBuffer (num_channels, buffer_size);
+    if (!db->is_ready ())
+    {
+        safe_logger (spdlog::level::err, "unable to prepare buffer");
+        delete db;
+        db = NULL;
+        return INVALID_BUFFER_SIZE_ERROR;
+    }
+
+    // need to start impedance streaming
+    if (is_cheking_impedance)
+    {
+        res = send_config ("z");
+        if (res == STATUS_OK)
+        {
+            keep_alive = true;
+            streaming_thread = std::thread ([this] { this->read_thread_impedance (); });
+            return STATUS_OK;
+        }
+        else
+        {
+            return res;
+        }
+    }
+    // start plain streaming
+    else
+    {
+        std::string url = "http://" + params.ip_address + "/stream/start";
+        http_t *request = http_get (url.c_str (), NULL);
+        if (!request)
+        {
+            safe_logger (spdlog::level::err, "error during request creation, to {}", url.c_str ());
+            return GENERAL_ERROR;
+        }
+        int send_res = wait_for_http_resp (request);
+        if (send_res != STATUS_OK)
+        {
+            http_release (request);
+            return send_res;
+        }
+        http_release (request);
+
+        keep_alive = true;
+        streaming_thread = std::thread ([this] { this->read_thread (); });
+        return STATUS_OK;
+    }
+    return STATUS_OK;
+}
+
+int GanglionWifi::stop_stream ()
+{
+    if (keep_alive)
+    {
+        keep_alive = false;
+        streaming_thread.join ();
+        if (streamer)
+        {
+            delete streamer;
+            streamer = NULL;
+        }
+        // Board fails to receive/handle "Z" almost all the time
+        // workaround - still send 'Z' and send /stream/stop after that, maybe it will work
+        if (is_cheking_impedance)
+        {
+            send_config ("Z");
+        }
+        std::string url = "http://" + params.ip_address + "/stream/stop";
+        http_t *request = http_get (url.c_str (), NULL);
+        if (!request)
+        {
+            safe_logger (spdlog::level::err, "error during request creation, to {}", url.c_str ());
+            return GENERAL_ERROR;
+        }
+        int send_res = wait_for_http_resp (request);
+        if (send_res != STATUS_OK)
+        {
+            http_release (request);
+            return send_res;
+        }
+        http_release (request);
+
+        return STATUS_OK;
+    }
+    else
+    {
+        return STREAM_THREAD_IS_NOT_RUNNING;
+    }
+}
 
 void GanglionWifi::read_thread ()
 {
@@ -23,67 +171,151 @@ void GanglionWifi::read_thread ()
         Byte 33: 0xCX where X is 0-F in hex
     */
     int res;
-    unsigned char b[OpenBCIWifiShieldBoard::transaction_size];
+    unsigned char b[OpenBCIWifiShieldBoard::package_size];
+    double *package = new double[num_channels];
+    for (int i = 0; i < num_channels; i++)
+    {
+        package[i] = 0.0;
+    }
     while (keep_alive)
     {
         // check start byte
-        res = server_socket->recv (b, OpenBCIWifiShieldBoard::transaction_size);
-        if (res != OpenBCIWifiShieldBoard::transaction_size)
+        res = server_socket->recv (b, OpenBCIWifiShieldBoard::package_size);
+        if (res != OpenBCIWifiShieldBoard::package_size)
+        {
+            safe_logger (spdlog::level::warn, "recv result: {}", res);
+            if (res == -1)
+            {
+#ifdef _WIN32
+                safe_logger (spdlog::level::warn, "WSAGetLastError is {}", WSAGetLastError ());
+#else
+                safe_logger (spdlog::level::warn, "errno {} message {}", errno, strerror (errno));
+#endif
+            }
+
+            continue;
+        }
+
+        if (b[0] != START_BYTE)
         {
             continue;
         }
-        for (int cur_package = 0;
-             cur_package < OpenBCIWifiShieldBoard::num_packages_per_transaction; cur_package++)
+        if ((b[32] < END_BYTE_STANDARD) || (b[32] > END_BYTE_MAX))
         {
-            int offset = cur_package * OpenBCIWifiShieldBoard::package_size;
-            if (b[0 + offset] != START_BYTE)
-            {
-                continue;
-            }
-            if ((b[32 + offset] < END_BYTE_STANDARD) || (b[32 + offset] > END_BYTE_MAX))
-            {
-                safe_logger (spdlog::level::warn, "Wrong end byte, found {}", b[32 + offset]);
-                continue;
-            }
-
-            double package[18] = {0.};
-            // package num
-            package[0] = (double)b[1 + offset];
-            // eeg
-            for (int i = 0; i < 4; i++)
-            {
-                package[i + 1] = eeg_scale * cast_24bit_to_int32 (b + 2 + 3 * i + offset);
-            }
-            // end byte
-            package[8] = (double)b[32 + offset];
-            // place raw bytes to other_channels with end byte
-            package[9] = (double)b[26 + offset];
-            package[10] = (double)b[27 + offset];
-            package[11] = (double)b[28 + offset];
-            package[12] = (double)b[29 + offset];
-            package[13] = (double)b[30 + offset];
-            package[14] = (double)b[31 + offset];
-            // place accel data
-            if (b[32 + offset] == END_BYTE_STANDARD)
-            {
-                // accel
-                // mistake in firmware in axis
-                package[5] = accel_scale * cast_16bit_to_int32 (b + 28 + offset);
-                package[6] = accel_scale * cast_16bit_to_int32 (b + 26 + offset);
-                package[7] = -accel_scale * cast_16bit_to_int32 (b + 30 + offset);
-            }
-            // place analog data
-            if (b[32 + offset] == END_BYTE_ANALOG)
-            {
-                // analog
-                package[15] = cast_16bit_to_int32 (b + 26 + offset);
-                package[16] = cast_16bit_to_int32 (b + 28 + offset);
-                package[17] = cast_16bit_to_int32 (b + 30 + offset);
-            }
-
-            double timestamp = get_timestamp ();
-            db->add_data (timestamp, package);
-            streamer->stream_data (package, 18, timestamp);
+            safe_logger (spdlog::level::warn, "Wrong end byte, found {}", b[32]);
+            continue;
         }
+
+        // package num
+        package[0] = (double)b[1];
+        // eeg
+        for (int i = 0; i < 4; i++)
+        {
+            package[i + 1] = eeg_scale * cast_24bit_to_int32 (b + 2 + 3 * i);
+        }
+        // end byte
+        package[8] = (double)b[32];
+        // place raw bytes to other_channels with end byte
+        package[9] = (double)b[26];
+        package[10] = (double)b[27];
+        package[11] = (double)b[28];
+        package[12] = (double)b[29];
+        package[13] = (double)b[30];
+        package[14] = (double)b[31];
+        // place accel data
+        if (b[32] == END_BYTE_STANDARD)
+        {
+            // accel
+            // mistake in firmware in axis
+            package[5] = accel_scale * cast_16bit_to_int32 (b + 28);
+            package[6] = accel_scale * cast_16bit_to_int32 (b + 26);
+            package[7] = -accel_scale * cast_16bit_to_int32 (b + 30);
+        }
+        // place analog data
+        if (b[32] == END_BYTE_ANALOG)
+        {
+            // analog
+            package[15] = cast_16bit_to_int32 (b + 26);
+            package[16] = cast_16bit_to_int32 (b + 28);
+            package[17] = cast_16bit_to_int32 (b + 30);
+        }
+
+        double timestamp = get_timestamp ();
+        db->add_data (timestamp, package);
+        streamer->stream_data (package, num_channels, timestamp);
     }
+    delete[] package;
+}
+
+void GanglionWifi::read_thread_impedance ()
+{
+    int res;
+    unsigned char b[OpenBCIWifiShieldBoard::package_size];
+    double *package = new double[num_channels];
+    for (int i = 0; i < num_channels; i++)
+    {
+        package[i] = 0.0;
+    }
+    while (keep_alive)
+    {
+        // check start byte
+        res = server_socket->recv (b, OpenBCIWifiShieldBoard::package_size);
+        if (res != OpenBCIWifiShieldBoard::package_size)
+        {
+            safe_logger (spdlog::level::warn, "recv result: {}", res);
+            if (res == -1)
+            {
+#ifdef _WIN32
+                safe_logger (spdlog::level::warn, "WSAGetLastError is {}", WSAGetLastError ());
+#else
+                safe_logger (spdlog::level::warn, "errno {} message {}", errno, strerror (errno));
+#endif
+            }
+
+            continue;
+        }
+
+        if (b[0] != START_BYTE)
+        {
+            continue;
+        }
+        int channel_num = (int)(b[1]);
+        // parsing resistance value for channel as asci string
+        int val = 0;
+        int i = 0;
+        // find first non zero byte
+        for (i = 20; i > 2; i--)
+        {
+            if (b[i] != 0)
+            {
+                break;
+            }
+        }
+        std::string asci_value ((const char *)(b + 2), i - 2);
+        try
+        {
+            val = std::stoi (asci_value);
+        }
+        catch (...)
+        {
+            safe_logger (
+                spdlog::level::err, "failed to parse impedance data: {}", asci_value.c_str ());
+            continue;
+        }
+
+        // channel_num starts from 1, first resistance channel is 18
+        safe_logger (
+            spdlog::level::trace, "resistance value is {}, channel number is {}", val, channel_num);
+        if (!((channel_num > 0) && (channel_num < 6)))
+        {
+            safe_logger (spdlog::level::warn, "channel number is {}", channel_num);
+            continue;
+        }
+        package[18 + channel_num - 1] = val;
+
+        double timestamp = get_timestamp ();
+        db->add_data (timestamp, package);
+        streamer->stream_data (package, num_channels, timestamp);
+    }
+    delete[] package;
 }
