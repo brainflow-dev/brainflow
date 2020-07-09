a54f8c3acd61d49916ff3601c888a468bc4ae909
Andrey Parfenov <a1994ndrey@gmail.com>
2020-07-09 12:05:13 +0300

    typo fix
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- src/utils/inc/socket_client_udp.h
+++ src/utils/inc/socket_client_udp.h
@@ -28,7 +28,7 @@ class SocketClientUDP
 public:
     static int get_local_ip_addr (char *connect_ip, int port, char *local_ip);
 
-    SocketClientUDP (const char *port_name, int port);
+    SocketClientUDP (const char *ip_addr, int port);
     ~SocketClientUDP ()
     {
         close ();
