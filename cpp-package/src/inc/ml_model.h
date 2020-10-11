146152da2467737e716c7a296d4a5a6502212396
John42506176Linux <johnkongtcheu@gmail.com>
2020-10-11 12:39:41 -0400

    Adding to High level API

--- cpp-package/src/inc/ml_model.h
+++ cpp-package/src/inc/ml_model.h
@@ -15,11 +15,19 @@ class MLModel
 private:
     struct BrainFlowModelParams params;
     std::string serialized_params;
-
+    static void set_log_level (int log_level);
+    static void set_log_file (char *log_file);
 public:
     // clang-format off
     MLModel (struct BrainFlowModelParams params);
     ~MLModel ();
+
+    /// enable BrainFlow logger with LEVEL_INFO
+    static void enable_board_logger ();
+    /// disable BrainFlow loggers
+    static void disable_board_logger ();
+    /// enable BrainFlow logger with LEVEL_TRACE
+    static void enable_dev_board_logger ();
 
     /// initialize classifier, should be called first
     void prepare ();
