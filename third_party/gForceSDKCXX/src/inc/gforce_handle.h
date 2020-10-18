5aedb222739b4e5e36408794b8340bc78a84d1d3
Andrey Parfenov <a1994ndrey@gmail.com>
2020-10-18 17:56:46 +0300

    wip
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- third_party/gForceSDKCXX/src/inc/gforce_handle.h
+++ third_party/gForceSDKCXX/src/inc/gforce_handle.h
@@ -1,10 +1,47 @@
-#pragma once
+/*
+ * Copyright 2017, OYMotion Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ *
+ */
 
-#include <atlstr.h>
 #include <atomic>
+#include <functional>
 
 #include "gforce.h"
-#include "serial_detect.h"
+
+#include "gforce_wrapper_types.h"
+
+using namespace gf;
+using namespace std;
+
+#ifdef DEBUG
+#include <iostream>
+#endif
 
 
 class GForceHandle : public HubListener
@@ -12,34 +49,34 @@ class GForceHandle : public HubListener
 public:
     GForceHandle (gfsPtr<Hub> &pHub) : mHub (pHub)
     {
-        InitializeCriticalSection (&mCriticalSection);
+        iExitCode = (int)GforceWrapperExitCodes::SYNC_ERROR;
     }
+
     /// This callback is called when the Hub finishes scanning devices.
     virtual void onScanFinished () override
     {
-        if (mDevice == nullptr)
+        if (nullptr == mDevice)
         {
-            mExitCode = (int)CustomExitCodes::NO_DEVICE_FOUND;
+            iExitCode = (int)GforceWrapperExitCodes::NO_DEVICE_FOUND;
         }
         else
         {
             DeviceConnectionStatus status = mDevice->getConnectionStatus ();
-
             if (DeviceConnectionStatus::Connected != status &&
                 DeviceConnectionStatus::Connecting != status)
             {
-                if (GF_RET_CODE::GF_SUCCESS != mDevice->connect ())
+                if (GF_RET_CODE::GF_SUCCESS == mDevice->connect ())
                 {
-                    mExitCode = (int)CustomExitCodes::CONNECT_ERROR;
+                    iExitCode = (int)GforceWrapperExitCodes::STATUS_OK;
                 }
                 else
                 {
-                    mExitCode = (int)CustomExitCodes::STATUS_OK;
+                    iExitCode = (int)GforceWrapperExitCodes::CONNECT_ERROR;
                 }
             }
             else
             {
-                mExitCode = (int)CustomExitCodes::STATUS_OK;
+                iExitCode = (int)GforceWrapperExitCodes::FOUND_BUT_IN_CONNECTING_STATE;
             }
         }
     }
@@ -47,11 +84,7 @@ public:
     /// This callback is called when the state of the hub changed
     virtual void onStateChanged (HubState state) override
     {
-        if (HubState::Disconnected == state)
-        {
-            mLoop = false;
-            mDevice.reset ();
-        }
+        // do nothing
     }
 
     /// This callback is called when the hub finds a device.
@@ -71,76 +104,58 @@ public:
     /// This callback is called a device has been connected successfully
     virtual void onDeviceConnected (SPDEVICE device) override
     {
-        if (device)
+        if (nullptr != device)
         {
-            static auto setting = device->getDeviceSetting ();
-
-            if (nullptr != setting)
+            gfsPtr<DeviceSetting> ds = device->getDeviceSetting ();
+            if (nullptr != ds)
             {
-                setting->setDataNotifSwitch (
-                    (DeviceSetting::DataNotifFlags) (
-                        DeviceSetting::DNF_OFF | DeviceSetting::DNF_EMG_RAW),
-                    [](ResponseResult result) {
-                        std::string ret =
-                            (result == ResponseResult::RREST_SUCCESS) ? ("success") : ("failed");
-                    });
-
-                int ret = tryConfigEMG (setting, c_emgConfig[g_EMGchoice].sampleRate,
-                    c_emgConfig[g_EMGchoice].resolution);
-                if (0 == ret)
-                {
-                    bGetNewFile = true;
-                }
-                else
-                {
-                    if (0 != g_EMGchoice)
-                        ret = tryConfigEMG (
-                            setting, c_emgConfig[0].sampleRate, c_emgConfig[0].resolution);
-                    if (0 == ret)
-                        bGetNewFile = true;
-                    else
-                    {
-                        gShouldExit.store (true);
-                    }
-                }
+                ds->getFeatureMap (std::bind (&GForceHandle::featureCallback, this, ds,
+                    std::placeholders::_1, std::placeholders::_2));
             }
         }
     }
 
-    /// This callback is called when a device has been disconnected from
-    ///                                 connection state or failed to connect to
+    /// This callback is called when a device has been disconnected from connection state or failed
+    /// to connect to
     virtual void onDeviceDisconnected (SPDEVICE device, GF_UINT8 reason) override
     {
-        if (g_file.is_open ())
+        // if connection lost, we will try to reconnect again.
+        if (nullptr != device && device == mDevice)
         {
-            g_file.close ();
+            mDevice->connect ();
         }
-        mDevice.reset ();
-        mHub->startScan (DEFAULT_RSSI_THRESHOLD);
     }
 
     /// This callback is called when the quaternion data is received
     virtual void onOrientationData (SPDEVICE device, const Quaternion &rotation) override
     {
-        // ignore this kind of data
+        // dont use this data
     }
 
     /// This callback is called when the gesture data is recevied
     virtual void onGestureData (SPDEVICE device, Gesture gest) override
     {
-        // ignore this kind of data
+        // dont use this data
     }
 
     /// This callback is called when the button on gForce is pressed by user
     virtual void onDeviceStatusChanged (SPDEVICE device, DeviceStatus status) override
     {
-        // ignore it
+        // do nothing
     }
 
     /// This callback is called when extended data is received
     virtual void onExtendedDeviceData (
         SPDEVICE device, DeviceDataType dataType, gfsPtr<const std::vector<GF_UINT8>> data) override
     {
+#ifdef DEBUG
+        cout << __FUNCTION__ << ": datatype = " << (GF_UINT32)dataType
+             << ", datalength = " << data->size () << ", first byte: " << hex
+             << (GF_UINT32) ((data->size () > 0) ? data->at (0) : 0xFF) << ", last byte: "
+             << (GF_UINT32) ((data->size () > 0) ? data->at (data->size () - 1) : 0xFF) << dec
+             << endl;
+#endif
+
         if (data->size () == 0)
         {
             return;
@@ -148,84 +163,72 @@ public:
 
         switch (dataType)
         {
+            case DeviceDataType::DDT_ACCELERATE:
+                break;
+
+            case DeviceDataType::DDT_GYROSCOPE:
+                break;
+
+            case DeviceDataType::DDT_MAGNETOMETER:
+                break;
+
+            case DeviceDataType::DDT_EULERANGLE:
+                break;
+
+            case DeviceDataType::DDT_QUATERNION:
+                break;
+
+            case DeviceDataType::DDT_ROTATIONMATRIX:
+                break;
+
+            case DeviceDataType::DDT_GESTURE:
+                break;
+
             case DeviceDataType::DDT_EMGRAW:
-            {
-                time_t rawtime;
-                time (&rawtime);
-                // EnterCriticalSection(&mCriticalSection);
-                // save raw data with format
-                if (g_file.is_open () && bRecording)
-                {
-                    // format data:0x8192 + CheckSum + PID + RawData + Time
-                    g_file.put ((GF_UINT8)0x92); // magic number:0x8192
-                    g_file.put ((GF_UINT8)0x81); // magic number:0x8192
-                    g_file.put (CheckSum (&(*data)[0], (GF_UINT8)data->size ()));
-                    static GF_UINT8 pid = 0;
-                    g_file.put (pid);
-                    pid++;
-                    g_file.write ((const char *)&(*data)[0], data->size ());
-                    ULARGE_INTEGER ltime;
-                    ltime = GetFileTime ();
-                    g_file.write ((const char *)&ltime, sizeof (ltime));
-                    printSingleFileRecordedBytes ((int)g_file.tellp ());
-                }
-                // LeaveCriticalSection(&mCriticalSection);
-                gMessageQueue.push (data);
-            }
-            break;
-            default:
+                break;
+
+            case DeviceDataType::DDT_HIDMOUSE:
+                break;
+
+            case DeviceDataType::DDT_HIDJOYSTICK:
+                break;
+
+            case DeviceDataType::DDT_DEVICESTATUS:
                 break;
         }
     }
 
-    // Indicates if we want to exit app
-    bool getLoop ()
-    {
-        return mLoop;
-    }
-
-    void setLoop ()
-    {
-        mLoop = true;
-    }
-
-
 private:
-    CRITICAL_SECTION mCriticalSection;
-    // Indicates if we will keep message polling
-    bool mLoop = true;
-    // keep a instance of hub.
+    // return exit code
+    int iExitCode;
+    // keep an instance of hub.
     gfsPtr<Hub> mHub;
     // keep a device to operate
     gfsPtr<Device> mDevice;
-    // exit code
-    int mExitCode;
 
-    int tryConfigEMG (gfsPtr<DeviceSetting> &setting, GF_UINT16 samRate, GF_UINT8 resol)
+    void featureCallback (gfsPtr<DeviceSetting> ds, ResponseResult res, GF_UINT32 featureMap)
     {
+        featureMap >>= 6; // Convert feature map to notification flags
 
-        ResponseResult ret = ResponseResult::RREST_FAILED;
-        for (int trytime = 0; trytime < 3; ++trytime)
-        {
-            std::promise<ResponseResult> emgPromise;
-            auto emgFuture = emgPromise.get_future ();
-            setting->setEMGRawDataConfig (samRate,            // sample rate
-                (DeviceSetting::EMGRowDataChannels) (0x00FF), // channel 0~7
-                128,                                          // data length
-                resol,                                        // resolution
-                [&emgPromise](ResponseResult result) {
-                    string retLog =
-                        (result == ResponseResult::RREST_SUCCESS) ? ("sucess") : ("failed");
-                    emgPromise.set_value (result);
-                });
-            if (ResponseResult::RREST_SUCCESS == emgFuture.get ())
-            {
-                ret = ResponseResult::RREST_SUCCESS;
-                break;
-            }
-        }
-        if (ResponseResult::RREST_SUCCESS == ret)
-            return 0;
-        return -1;
+        DeviceSetting::DataNotifFlags flags = (DeviceSetting::DataNotifFlags) (
+            DeviceSetting::DNF_OFF | DeviceSetting::DNF_ACCELERATE | DeviceSetting::DNF_GYROSCOPE |
+            DeviceSetting::DNF_MAGNETOMETER
+            //| DeviceSetting::DNF_EULERANGLE
+            //| DeviceSetting::DNF_QUATERNION
+            //| DeviceSetting::DNF_ROTATIONMATRIX
+            //| DeviceSetting::DNF_EMG_GESTURE
+            | DeviceSetting::DNF_EMG_RAW
+            //| DeviceSetting::DNF_HID_MOUSE
+            //| DeviceSetting::DNF_HID_JOYSTICK
+            | DeviceSetting::DNF_DEVICE_STATUS);
+
+        ds->setDataNotifSwitch (
+            (DeviceSetting::DataNotifFlags) (flags & featureMap), [](ResponseResult res) {});
+
+        ds->setEMGRawDataConfig (500,                     // sample rate
+            (DeviceSetting::EMGRowDataChannels) (0x00FF), // channel 0~7
+            128,                                          // data length
+            12,                                           // adc resolution
+            [](ResponseResult result) {});
     }
 };
--- third_party/gForceSDKCXX/src/wrapper.cpp
+++ third_party/gForceSDKCXX/src/wrapper.cpp
@@ -1,68 +1,71 @@
 #include <atomic>
+#include <thread>
 
 #include "gforce.h"
 
-#include "gforce_wrapper_function.h"
+#include "gforce_handle.h"
+#include "gforce_wrapper_functions.h"
 #include "gforce_wrapper_types.h"
-#include "thread_safe_queue.h"
 
 using namespace gf;
 using namespace std;
 
-volatile int iExitcode = (int)CustomExitCodes::SYNC_ERROR;
+volatile int iExitcode = (int)GforceWrapperExitCodes::SYNC_ERROR;
 volatile bool bShouldStopStream = true;
 volatile bool bInitialized = false;
 std::thread tReadThread;
+gfsPtr<Hub> pHub = HubManager::getHubInstance (_T("GForceBrainFlowWrapper"));
 
 int gforce_initialize (void *param)
 {
-    if (initialized)
+    if (bInitialized)
     {
-        return (int)CustomExitCodes::ALREADY_INITIALIZED;
+        return (int)GforceWrapperExitCodes::ALREADY_INITIALIZED;
     }
-    hub.init ();
-    bool ret = hub.run ();
-    if (!ret)
+    pHub->init ();
+    GF_UINT32 period = 10; // ms, timeout which I dont understand
+    GF_RET_CODE retCode = pHub->run (period);
+    if (retCode != GF_RET_CODE::GF_SUCCESS)
     {
-        return (int)CustomExitCodes::HUB_INIT_FAILED;
+        return (int)GforceWrapperExitCodes::HUB_INIT_FAILED;
     }
-    initialized.store (true);
+    bInitialized = true;
 
-    return (int)CustomExitCodes::STATUS_OK;
+    return (int)GforceWrapperExitCodes::STATUS_OK;
 }
 
 int gforce_start_streaming (void *param)
 {
-    if (!initialized)
+    if (!bInitialized)
     {
-        return (int)CustomExitCodes::NOT_INITIALIZED;
+        return (int)GforceWrapperExitCodes::NOT_INITIALIZED;
     }
-    return (int)CustomExitCodes::STATUS_OK;
+    return (int)GforceWrapperExitCodes::STATUS_OK;
 }
 
 int gforce_stop_streaming (void *param)
 {
-    if (!initialized)
+    if (!bInitialized)
     {
-        return (int)CustomExitCodes::NOT_INITIALIZED;
+        return (int)GforceWrapperExitCodes::NOT_INITIALIZED;
     }
-    return (int)CustomExitCodes::STATUS_OK;
+    return (int)GforceWrapperExitCodes::STATUS_OK;
 }
 
 int gforce_release (void *param)
 {
-    if (!initialized)
+    if (!bInitialized)
     {
-        return (int)CustomExitCodes::NOT_INITIALIZED;
+        return (int)GforceWrapperExitCodes::NOT_INITIALIZED;
     }
-    return (int)CustomExitCodes::STATUS_OK;
+    return (int)GforceWrapperExitCodes::STATUS_OK;
 }
 
 int gforce_get_data (void *param)
 {
-    if (!initialized)
+    if (!bInitialized)
     {
-        return (int)CustomExitCodes::NOT_INITIALIZED;
+        return (int)GforceWrapperExitCodes::NOT_INITIALIZED;
     }
-    return (int)CustomExitCodes::STATUS_OK;
+    return (int)GforceWrapperExitCodes::STATUS_OK;
 }
