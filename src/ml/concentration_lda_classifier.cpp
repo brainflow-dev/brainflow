6d31cb575180388e190a8d1722ed4e5b062d8de1
John42506176Linux <johnkongtcheu@gmail.com>
2020-10-27 13:40:56 -0400

    Fixing

--- src/ml/concentration_lda_classifier.cpp
+++ src/ml/concentration_lda_classifier.cpp
@@ -32,6 +32,5 @@ int ConcentrationLDAClassifier::predict (double *data, int data_len, double *out
 
 int ConcentrationLDAClassifier::release ()
 {
-    safe_logger (spdlog::level::info, "Model has been cleared.");
     return (int)BrainFlowExitCodes::STATUS_OK;
 }
