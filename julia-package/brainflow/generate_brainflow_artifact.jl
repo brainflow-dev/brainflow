0dd153b2c40f6f238a4307749a8afa2f2d357af3
matcox <matthijs.cox@asml.com>
2020-12-18 14:22:38 +0100

    first artifact attempt

---
+++ julia-package/brainflow/generate_brainflow_artifact.jl
@@ -0,0 +1,47 @@
+using SHA
+using Pkg
+using Pkg.Artifacts
+using Tar
+
+function sha256sum(tarball_path)
+    return open(tarball_path, "r") do io
+        return bytes2hex(sha256(io))
+    end
+end
+
+function add_brainflow_artifact!(
+    url,
+    artifact_toml_path = "Artifacts.toml",
+    artifact_name = "brainflow",
+    )
+    
+    download_path = "$(tempname())-download.tar"
+    download(url, download_path)
+    tar_hash_sha256 = sha256sum(download_path)
+
+    brainflow_hash = create_artifact() do artifact_dir 
+        # Pkg.PlatformEngines.unpack() gives errors for some users on Windows
+        Tar.extract(download_path, artifact_dir)
+    end
+
+    rm(download_path)
+
+    Pkg.Artifacts.bind_artifact!(
+        artifact_toml_path, 
+        artifact_name, 
+        brainflow_hash; 
+        download_info=[(url, tar_hash_sha256)], 
+        force=true
+    )
+
+    return brainflow_hash
+end
+
+cd(@__DIR__)
+include("src/brainflow_url.jl")
+add_brainflow_artifact!(brainflow_url())
+
+# # try it out; remove artifact and try to retrieve it again
+# brainflow_hash = artifact_hash("brainflow", "Artifacts.toml")
+# remove_artifact(brainflow_hash)
+# brainflow_artifact_path = artifact"brainflow"
--- julia-package/brainflow/src/c_interfaces.jl
+++ julia-package/brainflow/src/c_interfaces.jl
@@ -1,3 +1,37 @@
+using Pkg
+using Pkg.Artifacts
+using SHA
+using Tar
+
+function download_brainflow_artifact()
+
+    url = brainflow_url()
+
+    download_path = "$(tempname())-download.tar"
+    download(url, download_path)
+
+    brainflow_hash = create_artifact() do artifact_dir 
+        # Pkg.PlatformEngines.unpack() gives errors for some users on Windows
+        Tar.extract(download_path, artifact_dir)
+    end
+
+    rm(download_path)
+
+    return brainflow_hash
+end
+
+function get_brainflow_artifact_path()
+    try
+        return artifact"brainflow"
+    catch
+        @warn "Could not automatically retrieve brainflow artifact, attempting manual download"
+        # we have a known issue with unpack(), so wrote a custom download
+        # https://discourse.julialang.org/t/unable-to-automatically-install-artifact/51984/2
+        download_brainflow_artifact()
+        return artifact"brainflow"
+    end
+end
+
 function interface_path(library::AbstractString)
     return abspath(INTERFACE_PATH, interface_name(library))
 end
@@ -16,7 +50,8 @@ end
 # using Pkg.Artifacts
 # const INTERFACE_PATH = artifact"brainflow"
 # Right now using a hardcoded path during refactoring, with manual /lib location
-const INTERFACE_PATH = abspath(joinpath(@__DIR__, "../lib"))
+#const INTERFACE_PATH = abspath(joinpath(@__DIR__, "../lib"))
+const INTERFACE_PATH = get_brainflow_artifact_path()
 
 # we should instead use dlopen(interface_path())?
 const DATA_HANDLER_INTERFACE = interface_path("DataHandler")
