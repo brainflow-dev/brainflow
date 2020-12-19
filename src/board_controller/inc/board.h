bfca65029a760d8eba3af27e3591748e04accd59
Andrey Parfenov <a1994ndrey@gmail.com>
2020-12-19 19:03:29 +0300

    fixing clang format
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- src/board_controller/inc/board.h
+++ src/board_controller/inc/board.h
@@ -58,8 +58,10 @@ public:
     // Board::board_logger should not be called from destructors, to ensure that there are safe log
     // methods Board::board_logger still available but should be used only outside destructors
     template <typename Arg1, typename... Args>
+    // clang-format off
     void safe_logger (
-        spdlog::level::level_enum log_level, const char *fmt, const Arg1 &arg1, const Args &...args)
+        spdlog::level::level_enum log_level, const char *fmt, const Arg1 &arg1, const Args &... args)
+    // clang-format on
     {
         if (!skip_logs)
         {
