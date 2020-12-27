529d472b4598b44c66f7d7849fe898f7d54338f1
dvaposto <3050571+dvaposto@users.noreply.github.com>
2020-12-27 16:50:12 +0300

    Fix windowing example for Java

--- java-package/brainflow/src/main/java/brainflow/examples/Windowing.java
+++ java-package/brainflow/src/main/java/brainflow/examples/Windowing.java
@@ -2,13 +2,15 @@ package brainflow.examples;
 
 import java.util.Arrays;
 
+import brainflow.DataFilter;
+
 public class Windowing
 {
 
     public static void main (String[] args) throws Exception
     {
         int window_len = 20;
-        for (int window_id = 0; i < 4; i++)
+        for (int window_id = 0; i < 4; window_id++)
         {
             double[] window_data = DataFilter.get_window (window_id, window_len);
             System.out.println ("Window data:");
