e3daf30c989d49c144725b7172d5f7a7d12be7fe
dvaposto <3050571+dvaposto@users.noreply.github.com>
2020-12-28 22:37:12 +0300

    Replace consts with enum

--- tests/python/windowing.py
+++ tests/python/windowing.py
@@ -3,31 +3,27 @@ import numpy as np
 
 import brainflow
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, LogLevels, BoardIds
-from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
+from brainflow.data_filter import DataFilter, FilterTypes, WindowFunctions
 
 
 def main ():
     # demo for data windowing
-    NO_WINDOW = 0
-    HANNING = 1
-    HAMMING = 2
-    BLACKMAN_HARRIS = 3
     window_len = 20
 
     for window_function in range(4):
 
-        if window_function == NO_WINDOW:
+        if window_function == WindowFunctions.NO_WINDOW.value:
             print ('Window data for NO_WINDOW function:')
-            window_data = DataFilter.get_window (NO_WINDOW, window_len)
-        elif window_function == HANNING:
+            window_data = DataFilter.get_window (WindowFunctions.NO_WINDOW.value, window_len)
+        elif window_function == WindowFunctions.HANNING.value:
             print ('Window data for HANNING function:')
-            window_data = DataFilter.get_window (HANNING, window_len)
-        elif window_function == HAMMING:
+            window_data = DataFilter.get_window (WindowFunctions.HANNING.value, window_len)
+        elif window_function == WindowFunctions.HAMMING.value:
             print ('Window data for HAMMING function:')
-            window_data = DataFilter.get_window (HAMMING, window_len)
+            window_data = DataFilter.get_window (WindowFunctions.HAMMING.value, window_len)
         else:
             print ('Window data for BLACKMAN_HARRIS function:')
-            window_data = DataFilter.get_window (BLACKMAN_HARRIS, window_len)
+            window_data = DataFilter.get_window (WindowFunctions.BLACKMAN_HARRIS.value, window_len)
 
         print (window_data)
 
