96b50765686945098f8bf4a1a2d2ec1b06f90fba
Andrey Parfenov <a1994ndrey@gmail.com>
2021-01-08 04:19:34 +0300

    add wnovarargs
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- matlab-package/brainflow/examples/BandPower.m
+++ matlab-package/brainflow/examples/BandPower.m
@@ -1,22 +1,22 @@
-BoardShim.set_log_file ('brainflow.log');
-BoardShim.enable_dev_board_logger ();
+BoardShim.set_log_file('brainflow.log');
+BoardShim.enable_dev_board_logger();
 
-params = BrainFlowInputParams ();
-board_shim = BoardShim (int32 (BoardIDs.SYNTHETIC_BOARD), params);
-sampling_rate = BoardShim.get_sampling_rate (int32 (BoardIDs.SYNTHETIC_BOARD));
-board_shim.prepare_session ();
-board_shim.start_stream (45000, '');
-pause (10);
-board_shim.stop_stream ();
-nfft = DataFilter.get_nearest_power_of_two (sampling_rate);
-data = board_shim.get_board_data ();
-board_shim.release_session ();
+params = BrainFlowInputParams();
+board_shim = BoardShim(int32(BoardIDs.SYNTHETIC_BOARD), params);
+sampling_rate = BoardShim.get_sampling_rate(int32(BoardIDs.SYNTHETIC_BOARD));
+board_shim.prepare_session();
+board_shim.start_stream(45000, '');
+pause(10);
+board_shim.stop_stream();
+nfft = DataFilter.get_nearest_power_of_two(sampling_rate);
+data = board_shim.get_board_data();
+board_shim.release_session();
 
-eeg_channels = BoardShim.get_eeg_channels (int32 (BoardIDs.SYNTHETIC_BOARD));
-eeg_channel = eeg_channels (2);
-original_data = data (eeg_channel, :);
-detrended = DataFilter.detrend (original_data, int32 (DetrendOperations.LINEAR));
-[ampls, freqs] = DataFilter.get_psd_welch (detrended, nfft, nfft / 2, sampling_rate, int32 (WindowFunctions.HANNING));
-band_power_alpha = DataFilter.get_band_power (ampls, freqs, 7.0, 13.0);
-band_power_beta = DataFilter.get_band_power (ampls, freqs, 14.0, 30.0);
-ratio = band_power_alpha / band_power_beta;
+eeg_channels = BoardShim.get_eeg_channels(int32(BoardIDs.SYNTHETIC_BOARD));
+eeg_channel = eeg_channels(2);
+original_data = data(eeg_channel, :);
+detrended = DataFilter.detrend(original_data, int32(DetrendOperations.LINEAR));
+[ampls, freqs] = DataFilter.get_psd_welch(detrended, nfft, nfft / 2, sampling_rate, int32(WindowFunctions.HANNING));
+band_power_alpha = DataFilter.get_band_power(ampls, freqs, 7.0, 13.0);
+band_power_beta = DataFilter.get_band_power(ampls, freqs, 14.0, 30.0);
+ratio = band_power_alpha / band_power_beta
