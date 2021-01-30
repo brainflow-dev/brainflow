411fddc54b0d94954f0a553e820431ca8a277f31
dvaposto <3050571+dvaposto@users.noreply.github.com>
2021-01-30 23:48:21 +0300

    Rm redundant imports

--- tests/python/csp.py
+++ tests/python/csp.py
@@ -1,10 +1,7 @@
 import numpy as np
 
 import brainflow
-from brainflow.board_shim import BoardShim, BrainFlowInputParams, LogLevels, BoardIds
-from brainflow.data_filter import DataFilter, FilterTypes, WindowFunctions
-
-from pathlib import Path
+from brainflow.data_filter import DataFilter
 
 
 def main():
@@ -13,16 +10,17 @@ def main():
     labels = np.array([0, 1]).astype('float64') 
     
     data = np.array([[[6, 3, 1, 5], [3, 0, 5, 1]], [[1, 5, 6, 2], [5, 1, 2, 2]]]).astype('float64')
-    
-    filters, ev = DataFilter.get_csp(data, labels);
+
+    filters, eigvals = DataFilter.get_csp(data, labels);
+
+    true_filters = np.array([[-0.313406, 0.0792153], [-0.280803, -0.480046]])
 
     print("filters = ")
     print(filters)
 
-    true_filters = np.array([[-0.313406, 0.0792153], [-0.280803, -0.480046]])
-
     print("true filters = ")
     print(true_filters)
+
 
 if __name__ == "__main__":
     main()
