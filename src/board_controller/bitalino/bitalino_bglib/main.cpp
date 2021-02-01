9e7694dc7c6919803497a838f340125b6436cc4d
Andrey Parfenov <a1994ndrey@gmail.com>
2021-02-02 02:50:31 +0300

    fixing ci
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- src/board_controller/bitalino/bitalino_bglib/main.cpp
+++ src/board_controller/bitalino/bitalino_bglib/main.cpp
@@ -105,7 +105,7 @@ namespace BitalinoLib
         state = State::OPEN_CALLED;
         char *mac_addr = (char *)param;
         // convert string mac addr to bd_addr struct
-        for (int i = 0; i < strlen (mac_addr); i++)
+        for (unsigned int i = 0; i < strlen (mac_addr); i++)
         {
             mac_addr[i] = tolower (mac_addr[i]);
         }
