56b445915328ccafc3fe7fc1cead1ad19320656c
Andrey Parfenov <a1994ndrey@gmail.com>
2021-02-16 15:19:29 +0300

    refactor c# binding (#235)
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

---
+++ csharp-package/brainflow/tests/markers/markers.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+using brainflow;
+using brainflow.math;
+
+
+namespace test
+{
+    class Markers
+    {
+        static void Main (string[] args)
+        {
+            BoardShim.enable_dev_board_logger ();
+
+            BrainFlowInputParams input_params = new BrainFlowInputParams ();
+            int board_id = parse_args (args, input_params);
+
+            BoardShim board_shim = new BoardShim (board_id, input_params);
+            board_shim.prepare_session ();
+            board_shim.start_stream (450000, "file://file_stream.csv:w");
+            for (int i = 1; i < 5; i++)
+            {
+                System.Threading.Thread.Sleep (1000);
+                board_shim.insert_marker (i);
+            }
+            board_shim.stop_stream ();
+            board_shim.release_session ();
+        }
+
+        static int parse_args (string[] args, BrainFlowInputParams input_params)
+        {
+            int board_id = (int)BoardIds.SYNTHETIC_BOARD; //assume synthetic board by default
+            // use docs to get params for your specific board, e.g. set serial_port for Cyton
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].Equals ("--ip-address"))
+                {
+                    input_params.ip_address = args[i + 1];
+                }
+                if (args[i].Equals ("--mac-address"))
+                {
+                    input_params.mac_address = args[i + 1];
+                }
+                if (args[i].Equals ("--serial-port"))
+                {
+                    input_params.serial_port = args[i + 1];
+                }
+                if (args[i].Equals ("--other-info"))
+                {
+                    input_params.other_info = args[i + 1];
+                }
+                if (args[i].Equals ("--ip-port"))
+                {
+                    input_params.ip_port = Convert.ToInt32 (args[i + 1]);
+                }
+                if (args[i].Equals ("--ip-protocol"))
+                {
+                    input_params.ip_protocol = Convert.ToInt32 (args[i + 1]);
+                }
+                if (args[i].Equals ("--board-id"))
+                {
+                    board_id = Convert.ToInt32 (args[i + 1]);
+                }
+                if (args[i].Equals("--timeout"))
+                {
+                    input_params.timeout = Convert.ToInt32(args[i + 1]);
+                }
+                if (args[i].Equals("--serial-number"))
+                {
+                    input_params.serial_number = args[i + 1];
+                }
+                if (args[i].Equals ("--file"))
+                {
+                    input_params.file = args[i + 1];
+                }
+            }
+            return board_id;
+        }
+    }
+}
---
+++ csharp-package/brainflow/tests/transforms/transforms.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Numerics;
+
+using brainflow;
+using brainflow.math;
+
+
+namespace test
+{
+    class Transforms
+    {
+        static void Main (string[] args)
+        {
+            // use synthetic board for demo
+            BoardShim.enable_dev_board_logger ();
+            BrainFlowInputParams input_params = new BrainFlowInputParams ();
+            int board_id = (int)BoardIds.SYNTHETIC_BOARD;
+
+            BoardShim board_shim = new BoardShim (board_id, input_params);
+            board_shim.prepare_session ();
+            board_shim.start_stream (3600);
+            System.Threading.Thread.Sleep (5000);
+            board_shim.stop_stream ();
+            double[,] unprocessed_data = board_shim.get_current_board_data (64);
+            int[] eeg_channels = BoardShim.get_eeg_channels (board_id);
+            board_shim.release_session ();
+
+            for (int i = 0; i < eeg_channels.Length; i++)
+            {
+                Console.WriteLine ("Original data:");
+                Console.WriteLine ("[{0}]", string.Join (", ", unprocessed_data.GetRow (eeg_channels[i])));
+                // demo for wavelet transform
+                // tuple of coeffs array in format[A(J) D(J) D(J-1) ..... D(1)] where J is a
+                // decomposition level, A - app coeffs, D - detailed coeffs, and array which stores
+                // length for each block, len of this array is decomposition_length + 1
+                Tuple<double[], int[]> wavelet_data = DataFilter.perform_wavelet_transform(unprocessed_data.GetRow (eeg_channels[i]), "db4", 3);
+                // print app coeffs
+                for (int j = 0; j < wavelet_data.Item2[0]; j++)
+                {
+                    Console.Write (wavelet_data.Item1[j] + " ");
+                }
+                Console.WriteLine ();
+                // you can do smth with wavelet coeffs here, for example denoising works via thresholds for wavelets coeffs
+                double[] restored_data = DataFilter.perform_inverse_wavelet_transform (wavelet_data, unprocessed_data.GetRow (eeg_channels[i]).Length, "db4", 3);
+                Console.WriteLine ("Restored wavelet data:");
+                Console.WriteLine ("[{0}]", string.Join (", ", restored_data));
+
+                // demo for fft
+                // end_pos - start_pos must be a power of 2
+                Complex[] fft_data = DataFilter.perform_fft (unprocessed_data.GetRow (eeg_channels[i]), 0, 64, (int)WindowFunctions.HAMMING);
+                // len of fft_data is N / 2 + 1
+                double[] restored_fft_data = DataFilter.perform_ifft (fft_data);
+                Console.WriteLine ("Restored fft data:");
+                Console.WriteLine ("[{0}]", string.Join (", ", restored_fft_data));
+            }
+        }
+    }
+}
