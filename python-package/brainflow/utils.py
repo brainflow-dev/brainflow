57030120db536291bc65950f47230d39de3723fb
Andrey Parfenov <a1994ndrey@gmail.com>
2021-03-03 17:08:02 +0300

    fixing ci
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- python-package/brainflow/utils.py
+++ python-package/brainflow/utils.py
@@ -5,8 +5,6 @@ from brainflow.exit_codes import BrainflowExitCodes
 
 
 def check_memory_layout_row_major(data):
-    if not data:
-        raise BrainFlowError('empty data', BrainflowExitCodes.EMPTY_BUFFER_ERROR.value)
     if not data.flags['C_CONTIGUOUS']:
         raise BrainFlowError('wrong memory layout, should be row major, make sure you didnt tranpose array',
                              BrainflowExitCodes.INVALID_ARGUMENTS_ERROR.value)
