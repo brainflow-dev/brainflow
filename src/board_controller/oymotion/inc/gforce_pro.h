73c01ca641f54da5dda6205214a0c67033014df9
Andrey Parfenov <a1994ndrey@gmail.com>
2021-04-08 03:24:56 +0300

    fix linux compilation
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- src/board_controller/oymotion/inc/gforce_pro.h
+++ src/board_controller/oymotion/inc/gforce_pro.h
@@ -34,7 +34,7 @@ public:
     ~GforcePro ();
 
     int prepare_session ();
-    int start_stream ();
+    int start_stream (int buffer_size, char *streamer_params);
     int stop_stream ();
     int release_session ();
     int config_board (std::string config, std::string &response);
