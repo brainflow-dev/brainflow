45f4d9f9d8e135efd745cd13802afb9d38d975d4
Andrey Parfenov <a1994ndrey@gmail.com>
2021-05-23 00:12:29 +0300

    add real time example for python
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

---
+++ tests/python/plot_real_time.py
@@ -0,0 +1,99 @@
+import argparse
+import time
+import random
+
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
+
+import brainflow
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
+from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
+
+
+class Graph:
+    def __init__(self, board_shim):
+        self.board_id = board_shim.get_board_id()
+        self.board_shim = board_shim
+        self.exg_channels = BoardShim.get_exg_channels(self.board_id)
+        self.sampling_rate = BoardShim.get_sampling_rate(self.board_id)
+        self.update_speed_ms = 50
+        self.window_size = 5
+        self.num_points = self.window_size * self.sampling_rate
+
+        self.app = QtGui.QApplication([])
+        self.win = pg.GraphicsWindow()
+        self.win.setWindowTitle('EXG Plot')
+
+        self.plots = list()
+        self.curves = list()
+        for i in range(len(self.exg_channels)):
+            p = self.win.addPlot(colspan=2)
+            self.plots.append(p)
+            curve = p.plot()
+            self.curves.append(curve)
+            self.win.nextRow()
+
+        timer = QtCore.QTimer()
+        timer.timeout.connect(self.update)
+        timer.start(self.update_speed_ms)
+        QtGui.QApplication.instance().exec_()
+
+    def update(self):
+        data = self.board_shim.get_current_board_data(self.num_points)
+        for count, channel in enumerate(self.exg_channels):
+            DataFilter.perform_bandpass(data[channel], self.sampling_rate, 51.0, 100.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            DataFilter.perform_bandstop(data[channel], self.sampling_rate, 50.0, 4.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            DataFilter.perform_bandstop(data[channel], self.sampling_rate, 60.0, 4.0, 2,
+                                        FilterTypes.BUTTERWORTH.value, 0)
+            self.curves[count].setData(data[channel].tolist())
+        self.app.processEvents()  
+       
+
+
+def main():
+    BoardShim.enable_dev_board_logger()
+
+    parser = argparse.ArgumentParser()
+    # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
+    parser.add_argument('--timeout', type=int, help='timeout for device discovery or connection', required=False,
+                        default=0)
+    parser.add_argument('--ip-port', type=int, help='ip port', required=False, default=0)
+    parser.add_argument('--ip-protocol', type=int, help='ip protocol, check IpProtocolType enum', required=False,
+                        default=0)
+    parser.add_argument('--ip-address', type=str, help='ip address', required=False, default='')
+    parser.add_argument('--serial-port', type=str, help='serial port', required=False, default='')
+    parser.add_argument('--mac-address', type=str, help='mac address', required=False, default='')
+    parser.add_argument('--other-info', type=str, help='other info', required=False, default='')
+    parser.add_argument('--streamer-params', type=str, help='streamer params', required=False, default='')
+    parser.add_argument('--serial-number', type=str, help='serial number', required=False, default='')
+    parser.add_argument('--board-id', type=int, help='board id, check docs to get a list of supported boards',
+                        required=False, default=BoardIds.SYNTHETIC_BOARD)
+    parser.add_argument('--file', type=str, help='file', required=False, default='')
+    args = parser.parse_args()
+
+    params = BrainFlowInputParams()
+    params.ip_port = args.ip_port
+    params.serial_port = args.serial_port
+    params.mac_address = args.mac_address
+    params.other_info = args.other_info
+    params.serial_number = args.serial_number
+    params.ip_address = args.ip_address
+    params.ip_protocol = args.ip_protocol
+    params.timeout = args.timeout
+    params.file = args.file
+
+    try:
+        board_shim = BoardShim(args.board_id, params)
+        board_shim.prepare_session()
+        board_shim.start_stream()
+        g = Graph(board_shim)
+    except BaseException as e:
+        print(e)
+        if board_shim.is_prepared():
+            board_shim.release_session()
+
+
+if __name__ == '__main__':
+    main()
