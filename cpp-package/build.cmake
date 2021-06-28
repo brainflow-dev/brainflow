d2f23d5e65b080d4e14c9ca45e8d79bbbc1c5097
Andrey Parfenov <a1994ndrey@gmail.com>
2021-06-28 10:08:59 +0300

    fixing ci
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- cpp-package/build.cmake
+++ cpp-package/build.cmake
@@ -20,6 +20,16 @@ target_include_directories (
 target_link_libraries (${BRAINFLOW_CPP_BINDING_NAME} PRIVATE ${BOARD_CONTROLLER_NAME} ${DATA_HANDLER_NAME} ${ML_MODULE_NAME})
 
 install (
+    FILES
+    ${CMAKE_HOME_DIRECTORY}/cpp-package/src/inc/data_filter.h
+    ${CMAKE_HOME_DIRECTORY}/cpp-package/src/inc/board_shim.h
+    ${CMAKE_HOME_DIRECTORY}/cpp-package/src/inc/ml_model.h
+    ${CMAKE_HOME_DIRECTORY}/src/utils/inc/brainflow_array.h
+    ${CMAKE_HOME_DIRECTORY}/src/utils/inc/brainflow_exception.h
+    DESTINATION inc
+)
+
+install (
     TARGETS ${BRAINFLOW_CPP_BINDING_NAME}
     EXPORT ${TARGETS_EXPORT_NAME}
     RUNTIME DESTINATION lib
