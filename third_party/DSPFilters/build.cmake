70cf59734098a79c849182cecd8df5f9c1e7cc48
Andrey Parfenov <a1994ndrey@gmail.com>
2021-06-28 04:22:11 +0300

    add missed file
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

---
+++ third_party/DSPFilters/build.cmake
@@ -0,0 +1,6 @@
+SET (DSPFILTERS "DSPFilters")
+aux_source_directory (${CMAKE_HOME_DIRECTORY}/third_party/DSPFilters/source DSPFILTERS_SOURCE_LIB)
+add_library (${DSPFILTERS} STATIC ${DSPFILTERS_SOURCE_LIB})
+target_include_directories (${DSPFILTERS} PUBLIC $<BUILD_INTERFACE:${CMAKE_HOME_DIRECTORY}/third_party/DSPFilters/include>)
+set_property (TARGET ${DSPFILTERS} PROPERTY POSITION_INDEPENDENT_CODE ON)
+target_compile_definitions(${DSPFILTERS} PRIVATE -DNDEBUG)
