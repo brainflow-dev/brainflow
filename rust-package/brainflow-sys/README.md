473e10c3fd53c865735fc922d5550ca15f6b48a6
Daniel Hahne <d@niel.re>
2021-09-23 22:24:37 +0200

    Add enums, remove namespace, add readme, add cmake options

---
+++ rust-package/brainflow-sys/README.md
@@ -0,0 +1,4 @@
+# About
+`brainflow-sys` is an FFI-Rust-binding to brainflow.
+
+Run with feature `generate_binging` to generate the actual binding src/lib.rs
--- rust-package/brainflow-sys/build.rs
+++ rust-package/brainflow-sys/build.rs
@@ -5,15 +5,27 @@ use std::{fmt::Display, path::Path};
 
 #[cfg(feature = "generate_binding")]
 fn generate_binding() {
+    const ALLOW_UNCONVENTIONALS: &'static str = "#![allow(non_camel_case_types)]\n";
+
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     let header_path = out_path.join("inc");
     let header_path = header_path.display();
 
     let bindings = bindgen::Builder::default()
+        .header(format!("{}/brainflow_constants.h", &header_path))
         .header(format!("{}/board_controller.h", &header_path))
         .header(format!("{}/board_info_getter.h", &header_path))
         .header(format!("{}/data_handler.h", &header_path))
         .header(format!("{}/ml_module.h", &header_path))
+        .raw_line(ALLOW_UNCONVENTIONALS)
+        .clang_arg("-std=c++11")
+        .clang_arg("-x")
+        .clang_arg("c++")
+        .default_enum_style(bindgen::EnumVariation::Rust {
+            non_exhaustive: false,
+        })
+        .rustified_non_exhaustive_enum("BrainFlowExitCodes")
+        .rustified_non_exhaustive_enum("BoardIds")
         .generate()
         .expect("Unable to generate bindings");
 
@@ -35,7 +47,37 @@ fn build() {
     );
 
     println!("cargo:info=Building Brainflow via CMake.");
-    let brainflow_build_dir = cmake::build(brainflow_path);
+
+    let use_libftdi = if cfg!(feature = "use_libfidi") {
+        "ON"
+    } else {
+        "OFF"
+    };
+
+    let use_openmp = if cfg!(feature = "use_openmp") {
+        "ON"
+    } else {
+        "OFF"
+    };
+
+    let build_oymotion_sdk = if cfg!(feature = "build_oymotion_sdk") {
+        "ON"
+    } else {
+        "OFF"
+    };
+
+    let build_bluetooth = if cfg!(feature = "build_bluetooth") {
+        "ON"
+    } else {
+        "OFF"
+    };
+
+    let brainflow_build_dir = cmake::Config::new(brainflow_path)
+        .define("USE_LIBFTDI", use_libftdi)
+        .define("USE_OPENMP", use_openmp)
+        .define("BUILD_OYMOTION_SDK", build_oymotion_sdk)
+        .define("BUILD_BLUETOOTH", build_bluetooth)
+        .build();
     link(brainflow_build_dir.display())
 }
 
