34d11a7949000c5dcb4085e2b7f6942152cde9d5
Andrey Parfenov <a1994ndrey@gmail.com>
2021-10-07 01:24:30 +0300

    add missed file
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

---
+++ third_party/SimpleBLE/include/simpleble_c/shared_export.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifdef _WIN32
+#define SHARED_EXPORT __declspec(dllexport)
+#define CALLING_CONVENTION __cdecl
+#else
+#define SHARED_EXPORT __attribute__((visibility("default")))
+#define CALLING_CONVENTION
+#endif
