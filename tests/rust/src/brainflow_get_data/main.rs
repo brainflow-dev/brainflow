8d1042a2017354b40d050204b809e77ec9561038
Andrey Parfenov <a1994ndrey@gmail.com>
2021-10-15 03:59:14 +0300

    wip, adding rust tests
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- tests/rust/src/brainflow_get_data/main.rs
+++ tests/rust/src/brainflow_get_data/main.rs
@@ -7,22 +7,15 @@ use brainflow::BoardId;
 
 fn main() {
     brainflow::board_shim::enable_dev_board_logger().unwrap();
-    brainflow::ml_model::enable_ml_logger().unwrap();
     let params = BrainFlowInputParamsBuilder::default()
-        .serial_port("/dev/ttyUSB0")
         .build();
-    let board = BoardShim::new(BoardId::CytonBoard, params).unwrap();
-
-    dbg!("board instantiated");
+    let board = BoardShim::new(BoardId::SyntheticBoard, params).unwrap();
 
     board.prepare_session().unwrap();
-
-    board.start_stream(20, "").unwrap();
+    board.start_stream(45000, "").unwrap();
     thread::sleep(Duration::from_secs(5));
     board.stop_stream().unwrap();
-
-    let cnt = dbg!(board.get_board_data_count().unwrap());
-    let data = board.get_current_board_data(cnt).unwrap();
+    let data = board.get_board_data(None).unwrap();
     board.release_session().unwrap();
 
     dbg!(data);
