baf16d3d0b8e94a8d8c5adb0f4a70c2f8f4408a9
Andrey Parfenov <a1994ndrey@gmail.com>
2021-11-04 19:28:28 +0300

    check that we dont close handle twice (#381)
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- src/board_controller/openbci/ganglion_bglib/inc/uart.h
+++ src/board_controller/openbci/ganglion_bglib/inc/uart.h
@@ -3,7 +3,7 @@
 
 void uart_list_devices ();
 int uart_find_serialport (char *name);
-int uart_open (char *port);
+int uart_open (const char *port);
 void uart_close ();
 int uart_tx (int len, unsigned char *data);
 int uart_rx (int len, unsigned char *data, int timeout_ms);
