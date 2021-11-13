38af9b83d4be748e556d11597555a67ab939d3f5
Andrey Parfenov <a1994ndrey@gmail.com>
2021-11-13 03:07:41 +0300

    bump julia version
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- julia-package/brainflow/src/brainflow_url.jl
+++ julia-package/brainflow/src/brainflow_url.jl
@@ -1,5 +1,5 @@
 # TODO: automatically generate by brainflow CICD
 function brainflow_url()
-    url = "https://github.com/brainflow-dev/brainflow/releases/download/4.7.1/compiled_libs.tar"
+    url = "https://github.com/brainflow-dev/brainflow/releases/download/4.8.0/compiled_libs.tar"
     return url
 end
