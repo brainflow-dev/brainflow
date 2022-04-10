9d728791cc1228c01d10d7dfd74ec1c0211ec592
Philip Pitts <philip.pitts@protonmail.com>
2022-04-09 20:44:26 -0700

    add google test framework

---
+++ tests/cpp/hello_test.cc
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
