4525f96906cd5156d5a47ce68fb6b702a2724bec
Scott Miller <miller.scott.thomas@gmail.com>
2022-05-15 20:45:41 -0400

    fixed and simplified script for building Swift pkg

--- swift-package/build_package_macOS.sh
+++ swift-package/build_package_macOS.sh
@@ -1,17 +1,10 @@
 
-export BRAINFLOW_VERSION=4.9.0
-export GITHUB_WORKSPACE=..
-
-mkdir -p $GITHUB_WORKSPACE/installed/macOS
-cd $GITHUB_WORKSPACE/tools
-cmake -G Ninja -DBRAINFLOW_VERSION=$BRAINFLOW_VERSION -DBUILD_BLUETOOTH=ON -DBUILD_TESTS=ON -DWARNINGS_AS_ERRORS=ON \
-  -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 -DCMAKE_INSTALL_PREFIX=$GITHUB_WORKSPACE/installed/macOS \
-  -DCMAKE_BUILD_TYPE=Release ..
-ninja
-ninja install
+cd ../tools
+python3 build.py
 cd ../swift-package
-xcodebuild -create-xcframework -library ../installed/macOS/lib/libBoardController.dylib -headers ../inc -output BoardController.xcframework
-xcodebuild -create-xcframework -library ../lib/libBrainBitLib.dylib  -output BrainBitLib.xcframework
-xcodebuild -create-xcframework -library ../lib/libDataHandler.dylib  -output DataHandler.xcframework
-xcodebuild -create-xcframework -library ../lib/libMLModule.dylib  -output MLModule.xcframework
+xcodebuild -create-xcframework -library ../installed/lib/libBoardController.dylib \
+  -headers ../installed/inc -output BoardController.xcframework
+xcodebuild -create-xcframework -library ../installed/lib/libBrainBitLib.dylib  -output BrainBitLib.xcframework
+xcodebuild -create-xcframework -library ../installed/lib/libDataHandler.dylib  -output DataHandler.xcframework
+xcodebuild -create-xcframework -library ../installed/lib/libMLModule.dylib  -output MLModule.xcframework
 
