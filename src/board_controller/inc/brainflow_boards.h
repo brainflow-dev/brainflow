7b2f9d5be733dafa628ca15b95fbcb83ffa06ddd
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-02 16:26:09 +0800

    Delete src directory

--- src/board_controller/inc/brainflow_boards.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "json.hpp"
-
-using json = nlohmann::json;
-
-extern json brainflow_boards_json;
--- src/utils/inc/shared_export.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef _WIN32
-#define SHARED_EXPORT __declspec(dllexport)
-#define CALLING_CONVENTION __cdecl
-#else
-#define SHARED_EXPORT __attribute__ ((visibility ("default")))
-#define CALLING_CONVENTION
-#endif
