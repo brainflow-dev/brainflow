dd927801309df430a26ee9f09930313bb9f9e83d
Andrey Parfenov <a1994ndrey@gmail.com>
2022-06-09 07:34:46 +0300

    move examples
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

---
+++ csharp-package/brainflow/examples/band_power/band_power.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+using brainflow;
+using brainflow.math;
+
+
+namespace examples
+{
+    class BandPower
+    {
+        static void Main (string[] args)
+        {
+            // use synthetic board for demo
+            BoardShim.enable_dev_board_logger ();
+            BrainFlowInputParams input_params = new BrainFlowInputParams ();
+            int board_id = (int)BoardIds.SYNTHETIC_BOARD;
+            BoardDescr board_descr = BoardShim.get_board_descr<BoardDescr> (board_id);
+            int sampling_rate = board_descr.sampling_rate;
+            int nfft = DataFilter.get_nearest_power_of_two (sampling_rate);
+
+            BoardShim board_shim = new BoardShim (board_id, input_params);
+            board_shim.prepare_session ();
+            board_shim.start_stream (3600);
+            System.Threading.Thread.Sleep (10000);
+            board_shim.stop_stream ();
+            double[,] data = board_shim.get_board_data ();
+            int[] eeg_channels = board_descr.eeg_channels;
+            // use second channel of synthetic board to see 'alpha'
+            int channel = eeg_channels[1];
+            board_shim.release_session ();
+            double[] detrend = DataFilter.detrend (data.GetRow (channel), (int)DetrendOperations.LINEAR);
+            Tuple<double[], double[]> psd = DataFilter.get_psd_welch (detrend, nfft, nfft / 2, sampling_rate, (int)WindowOperations.HANNING);
+            double band_power_alpha = DataFilter.get_band_power (psd, 7.0, 13.0);
+            double band_power_beta = DataFilter.get_band_power (psd, 14.0, 30.0);
+            Console.WriteLine ("Alpha/Beta Ratio:" + (band_power_alpha / band_power_beta));
+        }
+    }
+}
---
+++ src/tests/build.cmake
@@ -0,0 +1,62 @@
+SET (TESTS_EXE_NAME "brainflow_tests")
+
+# Show Google Test options
+MARK_AS_ADVANCED (CLEAR BUILD_GMOCK)
+MARK_AS_ADVANCED (CLEAR INSTALL_GTEST)
+MARK_AS_ADVANCED (CLEAR FETCHCONTENT_BASE_DIR)
+MARK_AS_ADVANCED (CLEAR FETCHCONTENT_FULLY_DISCONNECTED)
+MARK_AS_ADVANCED (CLEAR FETCHCONTENT_QUIET)
+MARK_AS_ADVANCED (CLEAR FETCHCONTENT_SOURCE_DIR_GOOGLETEST)
+MARK_AS_ADVANCED (CLEAR FETCHCONTENT_UPDATES_DISCONNECTED)
+MARK_AS_ADVANCED (CLEAR FETCHCONTENT_UPDATES_DISCONNECTED_GOOGLETEST)
+
+SET (BUILD_GMOCK ON CACHE BOOL "" FORCE)
+SET (INSTALL_GTEST ON CACHE BOOL "" FORCE)
+SET (FETCHCONTENT_QUIET ON CACHE BOOL "" FORCE)
+
+# For Windows: Prevent overriding compiler/linker settings
+SET (gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+include(FetchContent)
+FetchContent_Declare(
+    googletest
+    URL https://github.com/google/googletest/archive/HEAD.zip
+)
+
+FetchContent_MakeAvailable(googletest)
+
+enable_testing()
+
+SET (TESTS_SRC
+    ${CMAKE_HOME_DIRECTORY}/src/utils/bluetooth/bluetooth_functions.cpp
+    ${CMAKE_HOME_DIRECTORY}/src/tests/bluetooth/socket_bluetooth_test.cpp
+    ${CMAKE_HOME_DIRECTORY}/src/tests/bluetooth/bluetooth_functions_unittest.cpp
+)
+
+add_executable(
+    ${TESTS_EXE_NAME}
+    ${TESTS_SRC}
+)
+
+target_include_directories (
+    ${TESTS_EXE_NAME} PRIVATE
+    ${CMAKE_HOME_DIRECTORY}/src/utils/inc
+    ${CMAKE_HOME_DIRECTORY}/src/utils/bluetooth/inc
+    ${CMAKE_HOME_DIRECTORY}/src/tests/bluetooth/inc
+    ${CMAKE_HOME_DIRECTORY}/src/utils/bluetooth/macos_third_party
+)
+
+target_link_libraries(
+    ${TESTS_EXE_NAME} PRIVATE
+    gmock_main
+)
+
+set_target_properties (${TESTS_EXE_NAME}
+    PROPERTIES
+    ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_HOME_DIRECTORY}/build/tests
+    LIBRARY_OUTPUT_DIRECTORY ${CMAKE_HOME_DIRECTORY}/build/tests
+    RUNTIME_OUTPUT_DIRECTORY ${CMAKE_HOME_DIRECTORY}/build/tests
+)
+
+include(GoogleTest)
+gtest_discover_tests(${TESTS_EXE_NAME})
