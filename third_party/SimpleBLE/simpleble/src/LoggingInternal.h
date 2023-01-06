3084b61a483894e174a9d6e86537ea0d98fb53e1
Andrey Parfenov <a1994ndrey@gmail.com>
2023-01-06 02:41:53 +0100

    simpleble_winrt_2 (#590)
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>
    
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- third_party/SimpleBLE/simpleble/src/LoggingInternal.h
+++ third_party/SimpleBLE/simpleble/src/LoggingInternal.h
@@ -19,44 +19,44 @@
 
 #if SIMPLEBLE_LOG_LEVEL == SIMPLEDBUS_LOG_LEVEL_VERBOSE
 
-#define SIMPLEBLE_LOG_FATAL(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::FATAL, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_ERROR(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::ERROR, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_WARN(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::WARN, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_INFO(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::INFO, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_DEBUG(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::DEBUG, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_FATAL(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Fatal, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_ERROR(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Error, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_WARN(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Warn, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_INFO(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Info, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_DEBUG(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Debug, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
 #define SIMPLEBLE_LOG_VERBOSE(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::VERBOSE, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
 
 #elif SIMPLEBLE_LOG_LEVEL == SIMPLEDBUS_LOG_LEVEL_DEBUG
 
-#define SIMPLEBLE_LOG_FATAL(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::FATAL, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_ERROR(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::ERROR, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_WARN(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::WARN, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_INFO(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::INFO, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_DEBUG(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::DEBUG, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_FATAL(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Fatal, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_ERROR(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Error, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_WARN(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Warn, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_INFO(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Info, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_DEBUG(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Debug, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
 #define SIMPLEBLE_LOG_VERBOSE(msg)
 
 #elif SIMPLEBLE_LOG_LEVEL == SIMPLEDBUS_LOG_LEVEL_INFO
 
-#define SIMPLEBLE_LOG_FATAL(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::FATAL, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_ERROR(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::ERROR, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_WARN(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::WARN, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_INFO(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::INFO, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_FATAL(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Fatal, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_ERROR(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Error, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_WARN(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Warn, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_INFO(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Info, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
 #define SIMPLEBLE_LOG_DEBUG(msg) 
 #define SIMPLEBLE_LOG_VERBOSE(msg)
 
 #elif SIMPLEBLE_LOG_LEVEL == SIMPLEDBUS_LOG_LEVEL_WARN
 
-#define SIMPLEBLE_LOG_FATAL(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::FATAL, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_ERROR(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::ERROR, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_WARN(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::WARN, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_FATAL(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Fatal, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_ERROR(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Error, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_WARN(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Warn, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
 #define SIMPLEBLE_LOG_INFO(msg) 
 #define SIMPLEBLE_LOG_DEBUG(msg) 
 #define SIMPLEBLE_LOG_VERBOSE(msg)
 
 #elif SIMPLEBLE_LOG_LEVEL == SIMPLEDBUS_LOG_LEVEL_ERROR
 
-#define SIMPLEBLE_LOG_FATAL(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::FATAL, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
-#define SIMPLEBLE_LOG_ERROR(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::ERROR, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_FATAL(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Fatal, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_ERROR(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Error, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
 #define SIMPLEBLE_LOG_WARN(msg)
 #define SIMPLEBLE_LOG_INFO(msg) 
 #define SIMPLEBLE_LOG_DEBUG(msg) 
@@ -64,7 +64,7 @@
 
 #elif SIMPLEBLE_LOG_LEVEL == SIMPLEDBUS_LOG_LEVEL_FATAL
 
-#define SIMPLEBLE_LOG_FATAL(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::FATAL, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
+#define SIMPLEBLE_LOG_FATAL(msg) SimpleBLE::Logging::Logger::get()->log(SimpleBLE::Logging::Level::Fatal, "SimpleBLE", __FILE__, __LINE__, __func__, msg)
 #define SIMPLEBLE_LOG_ERROR(msg)
 #define SIMPLEBLE_LOG_WARN(msg)
 #define SIMPLEBLE_LOG_INFO(msg) 
