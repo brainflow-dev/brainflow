6131cc88d6ce2e1c4bc3dcbd89b7cd609dfded5c
Andrey Parfenov <a1994ndrey@gmail.com>
2023-08-18 02:22:22 +0200

    fix to small filename limit
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- src/board_controller/inc/file_streamer.h
+++ src/board_controller/inc/file_streamer.h
@@ -4,6 +4,7 @@
 
 #include "streamer.h"
 
+#define BRAINFLOW_FILE_NAME_LIMIT 512
 
 class FileStreamer : public Streamer
 {
@@ -16,7 +17,7 @@ public:
     void stream_data (double *data);
 
 private:
-    char file[128];
-    char file_mode[128];
+    char file[BRAINFLOW_FILE_NAME_LIMIT];
+    char file_mode[BRAINFLOW_FILE_NAME_LIMIT];
     FILE *fp;
 };
