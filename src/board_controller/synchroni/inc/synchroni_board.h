786de8ae2fe05769d3f56835e7d211436f6c2312
AranRhiod <ziyue.wang5@mail.mcgill.ca>
2024-08-12 13:37:39 -0400

    build.cmake update

--- src/board_controller/synchroni/inc/synchroni_board.h
+++ src/board_controller/synchroni/inc/synchroni_board.h
@@ -21,4 +21,10 @@ public:
     int release_session ();
     int config_board (std::string config, std::string &response);
     int config_board (std::string config);
+    void adapter_1_on_scan_found (simpleble_adapter_t adapter, simpleble_peripheral_t peripheral);
+    void read_data (simpleble_uuid_t service, simpleble_uuid_t characteristic, uint8_t *data,
+        size_t size, int channel_num);
+protected:
+    void SynchroniBoard::decompress_eeg_config(uint8_t *data, double *package);
+    
 };
--- src/board_controller/synchroni/synchroni_board.cpp
+++ src/board_controller/synchroni/synchroni_board.cpp
@@ -24,3 +24,30 @@ int SynchroniBoard::start_stream(int buffer_size, const char * streamer_params)
 {
 
 }
+
+void SynchroniBoard::read_data(simpleble_uuid_t service, simpleble_uuid_t characteristic, uint8_t *data,
+        size_t size, int channel_num)
+{
+    if (size != 2)
+    {
+        safe_logger (spdlog::level::warn, "unexpected number of bytes received: {}", size);
+        return;
+    }
+    int num_rows = board_descr["default"]["num_rows"];
+    double *package = new double[num_rows];
+
+    for (int i = 0; i < num_rows; i++){
+        package[i] = 0.0;
+    }
+    if (data[0] == 0xA)
+    {
+        if (data[1] == 0x1)
+        {
+
+        }
+    }
+}
+
+void SynchroniBoard::decompress_eeg_config(uint8_t *data, double *package){
+    
+}
