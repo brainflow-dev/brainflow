38c0bc878027ac686d7207d362d150ad380bf72b
Andrey Parfenov <a1994ndrey@gmail.com>
2024-08-12 23:45:35 +0200

    bump julia version
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- src/ml/generated/mindfulness_model.cpp
+++ src/ml/generated/mindfulness_model.cpp
@@ -1,6 +1,6 @@
 
 #include "mindfulness_model.h"
 // clang-format off
-const double mindfulness_coefficients[5] = {-1.0189175792169414,2.6253600819024174,-31.680396972687575,13.548491520885264,43.09228570067639};
+const double mindfulness_coefficients[5] = {-1.4060899708538128,2.597693987367105,-30.96470526503066,12.04593986553724,45.773017975354556};
 double mindfulness_intercept = 0.000000;
 // clang-format on
