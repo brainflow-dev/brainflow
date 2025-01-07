3b4beadd79c85d79e2fe38d07290cfa5cf72aaea
AranRhiod <ziyue.wang5@mail.mcgill.ca>
2025-01-07 11:51:44 -0500

    removing changes in brainflow_get_data.py

--- python_package/examples/tests/brainflow_get_data.py
+++ python_package/examples/tests/brainflow_get_data.py
@@ -37,22 +37,17 @@ def main():
     params.timeout = args.timeout
     params.file = args.file
     params.master_board = args.master_board
-    
+
     board = BoardShim(args.board_id, params)
-    
     board.prepare_session()
     board.start_stream ()
-    
-    time.sleep(0)
-    print("The following is data")
-    #data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
+    time.sleep(10)
+    # data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
     data = board.get_board_data()  # get all data and remove it from internal buffer
-    
-    print(data)
     board.stop_stream()
     board.release_session()
-    
-    
+
+    print(data)
 
 
 if __name__ == "__main__":
