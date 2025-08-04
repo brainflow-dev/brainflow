a8fa4e6b41ed8c15ad10a6c1dadd247a86b7118d
Benjamin Dieudonné <benjamin.dieudonne@kuleuven.be>
2025-08-04 10:58:13 +0200

    clang fix

--- src/board_controller/ant_neuro/ant_neuro.cpp
+++ src/board_controller/ant_neuro/ant_neuro.cpp
@@ -118,8 +118,8 @@ int AntNeuroBoard::prepare_session ()
                 (!expected_serial_number.empty () && current_serial != expected_serial_number))
             {
                 safe_logger (spdlog::level::info, 
-                    "Found non-matching amplifier (board name {}, serial {})",
-                    current_board_name, current_serial);
+                    "Found non-matching amplifier (board name {}, serial {})", current_board_name, 
+                        current_serial);
                 delete current_amp;
                 continue;
             }
@@ -145,8 +145,8 @@ int AntNeuroBoard::prepare_session ()
             std::string current_board_name = "AntNeuro" + current_amp->getType ();
             std::string current_serial = current_amp->getSerialNumber ();
             safe_logger (spdlog::level::info, 
-                "Found non-matching amplifier (board name {}, serial {})",
-                current_board_name, current_serial);
+                "Found non-matching amplifier (board name {}, serial {})", current_board_name, 
+                    current_serial);
             delete current_amp;
         }
 
