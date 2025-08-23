5e9029f1bbe015f4d9a232654db573e2f367fa9e
Andrey Parfenov <a1994ndrey@gmail.com>
2025-08-23 19:07:18 +0200

    Add support for Muse S Anthena device
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- java_package/brainflow/src/main/java/brainflow/BoardIds.java
+++ java_package/brainflow/src/main/java/brainflow/BoardIds.java
@@ -73,7 +73,8 @@ public enum BoardIds
     SYNCHRONI_PENTO_8_CHANNELS_BOARD(61),
     SYNCHRONI_UNO_1_CHANNELS_BOARD(62),
     OB3000_24_CHANNELS_BOARD(63),
-    BIOLISTENER_BOARD(64);
+    BIOLISTENER_BOARD(64),
+    MUSE_S_ANTHENA_BOARD(65);
 
     private final int board_id;
     private static final Map<Integer, BoardIds> bi_map = new HashMap<Integer, BoardIds> ();
