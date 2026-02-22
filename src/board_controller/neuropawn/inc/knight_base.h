4ec4724009ee649680122f187112b5180f888c43
NeuroPawn Admin <kevin@neuropawn.tech>
2026-02-22 00:27:28 -0800

    Patch/pr 795 refactor (#7)

---
+++ src/board_controller/neuropawn/inc/knight_base.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <set>
+#include <thread>
+
+#include "board.h"
+#include "board_controller.h"
+#include "serial.h"
+
+class KnightBase : public Board
+{
+
+protected:
+    volatile bool keep_alive;
+    bool initialized;
+    bool is_streaming;
+    std::thread streaming_thread;
+    Serial *serial;
+
+    int min_package_size;
+    int gain;
+
+    virtual int send_to_board (const char *msg);
+    virtual int send_to_board (const char *msg, std::string &response);
+    virtual std::string read_serial_response ();
+    virtual int open_port ();
+    virtual int set_port_settings ();
+    virtual void read_thread () = 0;
+
+private:
+    static const std::set<int> allowed_gains;
+
+public:
+    KnightBase (int board_id, struct BrainFlowInputParams params);
+    virtual ~KnightBase ();
+
+    virtual int prepare_session ();
+    virtual int start_stream (int buffer_size, const char *streamer_params);
+    virtual int stop_stream ();
+    virtual int release_session ();
+    virtual int config_board (std::string config, std::string &response);
+
+    static constexpr int start_byte = 0xA0;
+    static constexpr int end_byte = 0xC0;
+};
---
+++ src/board_controller/neuropawn/knight_base.cpp
@@ -0,0 +1,275 @@
+#include <math.h>
+#include <string.h>
+#include <vector>
+
+#include "custom_cast.h"
+#include "json.hpp"
+#include "knight_base.h"
+#include "serial.h"
+#include "timestamp.h"
+
+using json = nlohmann::json;
+
+constexpr int KnightBase::start_byte;
+constexpr int KnightBase::end_byte;
+const std::set<int> KnightBase::allowed_gains = {1, 2, 3, 4, 6, 8, 12};
+
+KnightBase::KnightBase (int board_id, struct BrainFlowInputParams params) : Board (board_id, params)
+{
+    serial = NULL;
+    is_streaming = false;
+    keep_alive = false;
+    initialized = false;
+    gain = 12; // default gain value
+}
+
+KnightBase::~KnightBase ()
+{
+    skip_logs = true;
+    release_session ();
+}
+
+int KnightBase::prepare_session ()
+{
+    if (initialized)
+    {
+        safe_logger (spdlog::level::info, "Session already prepared");
+        return (int)BrainFlowExitCodes::STATUS_OK;
+    }
+    if (params.serial_port.empty ())
+    {
+        safe_logger (spdlog::level::err, "serial port is empty");
+        return (int)BrainFlowExitCodes::INVALID_ARGUMENTS_ERROR;
+    }
+
+    // Parse gain from other_info if provided
+    if (!params.other_info.empty ())
+    {
+        try
+        {
+            json j = json::parse (params.other_info);
+            if (j.contains ("gain"))
+            {
+                int parsed_gain = j["gain"];
+                // Validate gain is one of allowed values
+                if (allowed_gains.count (parsed_gain))
+                {
+                    gain = parsed_gain;
+                    safe_logger (spdlog::level::info, "Knight board gain set to {}", gain);
+                }
+                else
+                {
+                    safe_logger (
+                        spdlog::level::err, "Invalid gain value {} in other_info", parsed_gain);
+                    return (int)BrainFlowExitCodes::INVALID_ARGUMENTS_ERROR;
+                }
+            }
+            else
+            {
+                safe_logger (spdlog::level::info, "No gain field in other_info, using default 12");
+            }
+        }
+        catch (json::parse_error &e)
+        {
+            safe_logger (spdlog::level::err, "Failed to parse JSON from other_info: {}", e.what ());
+            return (int)BrainFlowExitCodes::INVALID_ARGUMENTS_ERROR;
+        }
+        catch (json::exception &e)
+        {
+            safe_logger (
+                spdlog::level::err, "JSON exception while parsing other_info: {}", e.what ());
+            return (int)BrainFlowExitCodes::INVALID_ARGUMENTS_ERROR;
+        }
+    }
+
+    serial = Serial::create (params.serial_port.c_str (), this);
+    int port_open = open_port ();
+    if (port_open != (int)BrainFlowExitCodes::STATUS_OK)
+    {
+        delete serial;
+        serial = NULL;
+        return port_open;
+    }
+
+    int set_settings = set_port_settings ();
+    if (set_settings != (int)BrainFlowExitCodes::STATUS_OK)
+    {
+        delete serial;
+        serial = NULL;
+        return set_settings;
+    }
+
+    initialized = true;
+    return (int)BrainFlowExitCodes::STATUS_OK;
+}
+
+int KnightBase::start_stream (int buffer_size, const char *streamer_params)
+{
+    if (is_streaming)
+    {
+        safe_logger (spdlog::level::err, "Streaming thread already running");
+        return (int)BrainFlowExitCodes::STREAM_ALREADY_RUN_ERROR;
+    }
+    int res = prepare_for_acquisition (buffer_size, streamer_params);
+    if (res != (int)BrainFlowExitCodes::STATUS_OK)
+    {
+        return res;
+    }
+
+    serial->flush_buffer ();
+
+    keep_alive = true;
+    streaming_thread = std::thread ([this] { this->read_thread (); });
+    is_streaming = true;
+    return (int)BrainFlowExitCodes::STATUS_OK;
+}
+
+int KnightBase::stop_stream ()
+{
+    if (is_streaming)
+    {
+        keep_alive = false;
+        is_streaming = false;
+        if (streaming_thread.joinable ())
+        {
+            streaming_thread.join ();
+        }
+        return (int)BrainFlowExitCodes::STATUS_OK;
+    }
+    else
+    {
+        return (int)BrainFlowExitCodes::STREAM_THREAD_IS_NOT_RUNNING;
+    }
+}
+
+int KnightBase::release_session ()
+{
+    if (initialized)
+    {
+        if (is_streaming)
+        {
+            stop_stream ();
+        }
+        free_packages ();
+        initialized = false;
+    }
+    if (serial)
+    {
+        serial->close_serial_port ();
+        delete serial;
+        serial = NULL;
+    }
+    return (int)BrainFlowExitCodes::STATUS_OK;
+}
+
+int KnightBase::open_port ()
+{
+    if (serial->is_port_open ())
+    {
+        safe_logger (spdlog::level::err, "port {} already open", serial->get_port_name ());
+        return (int)BrainFlowExitCodes::PORT_ALREADY_OPEN_ERROR;
+    }
+
+    safe_logger (spdlog::level::info, "openning port {}", serial->get_port_name ());
+    int res = serial->open_serial_port ();
+    if (res < 0)
+    {
+        return (int)BrainFlowExitCodes::UNABLE_TO_OPEN_PORT_ERROR;
+    }
+    safe_logger (spdlog::level::trace, "port {} is open", serial->get_port_name ());
+    return (int)BrainFlowExitCodes::STATUS_OK;
+}
+
+int KnightBase::set_port_settings ()
+{
+    int res = serial->set_serial_port_settings (1000, false);
+    if (res < 0)
+    {
+        safe_logger (spdlog::level::err, "Unable to set port settings, res is {}", res);
+        return (int)BrainFlowExitCodes::SET_PORT_ERROR;
+    }
+    res = serial->set_custom_baudrate (115200);
+    if (res < 0)
+    {
+        safe_logger (spdlog::level::err, "Unable to set custom baud rate, res is {}", res);
+        return (int)BrainFlowExitCodes::SET_PORT_ERROR;
+    }
+    safe_logger (spdlog::level::trace, "set port settings");
+    return (int)BrainFlowExitCodes::STATUS_OK;
+}
+
+int KnightBase::config_board (std::string config, std::string &response)
+{
+    if (!initialized)
+    {
+        return (int)BrainFlowExitCodes::BOARD_NOT_READY_ERROR;
+    }
+    int res = (int)BrainFlowExitCodes::STATUS_OK;
+    if (is_streaming)
+    {
+        safe_logger (spdlog::level::warn,
+            "You are changing board params during streaming, it may lead to sync mismatch between "
+            "data acquisition thread and device");
+        res = send_to_board (config.c_str ());
+    }
+    else
+    {
+        // read response if streaming is not running
+        res = send_to_board (config.c_str (), response);
+    }
+
+    return res;
+}
+
+int KnightBase::send_to_board (const char *msg)
+{
+    int length = (int)strlen (msg);
+    safe_logger (spdlog::level::debug, "sending {} to the board", msg);
+    int res = serial->send_to_serial_port ((const void *)msg, length);
+    if (res != length)
+    {
+        return (int)BrainFlowExitCodes::BOARD_WRITE_ERROR;
+    }
+
+    return (int)BrainFlowExitCodes::STATUS_OK;
+}
+
+int KnightBase::send_to_board (const char *msg, std::string &response)
+{
+    int length = (int)strlen (msg);
+    safe_logger (spdlog::level::debug, "sending {} to the board", msg);
+    int res = serial->send_to_serial_port ((const void *)msg, length);
+    if (res != length)
+    {
+        response = "";
+        return (int)BrainFlowExitCodes::BOARD_WRITE_ERROR;
+    }
+    response = read_serial_response ();
+
+    return (int)BrainFlowExitCodes::STATUS_OK;
+}
+
+std::string KnightBase::read_serial_response ()
+{
+    constexpr int max_tmp_size = 4096;
+    unsigned char tmp_array[max_tmp_size];
+    unsigned char tmp;
+    int tmp_id = 0;
+    while (serial->read_from_serial_port (&tmp, 1) == 1)
+    {
+        if (tmp_id < max_tmp_size)
+        {
+            tmp_array[tmp_id] = tmp;
+            tmp_id++;
+        }
+        else
+        {
+            serial->flush_buffer ();
+            break;
+        }
+    }
+    tmp_id = (tmp_id == max_tmp_size) ? tmp_id - 1 : tmp_id;
+    tmp_array[tmp_id] = '\0';
+
+    return std::string ((const char *)tmp_array);
+}
