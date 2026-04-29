9eea0b9de34f44371740591ea29654a71f573adf
Vignesh <v.rajan@amrc.co.uk>
2026-04-29 15:33:57 +0100

    Fix more Shimmer3 Board ID in more spots

--- src/board_controller/brainflow_boards.cpp
+++ src/board_controller/brainflow_boards.cpp
@@ -1199,7 +1199,7 @@ BrainFlowBoards::BrainFlowBoards()
         {"optical_channels", {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}},
         {"battery_channel", 17}
     };
-    // Shimmer3 (ID 67)
+    // Shimmer3 (ID 68)
     // Default preset: IMU data (accel, gyro, mag)
     brainflow_boards_json["boards"]["68"]["default"] =
     {
